77d428c32da9f59af5c56234a30fdcb120060698
johnche <johnche@tencent.com>
2020-08-27 14:13:58 +0800

    只引用，不生成的类型，应该也要声明其父类，否则继承链中有些类没配置就导致继承链断了。

--- unity/Assets/Puerts/Src/Editor/Generator.cs
+++ unity/Assets/Puerts/Src/Editor/Generator.cs
@@ -488,7 +488,7 @@ namespace Puerts.Editor
                 }
             }
 
-            if (genTypeSet.Contains(type) && !type.IsEnum && type.BaseType != null && type != typeof(object) && !result.IsDelegate && !result.IsInterface)
+            if (!type.IsEnum && type.BaseType != null && type != typeof(object) && !result.IsDelegate && !result.IsInterface)
             {
                 result.BaseType = new TsTypeGenInfo()
                 {
@@ -568,6 +568,13 @@ namespace Puerts.Editor
                     AddRefType(refTypes, gt);
                 }
             }
+
+            var baseType = type.BaseType;
+            while (baseType != null)
+            {
+                AddRefType(refTypes, baseType);
+                baseType = baseType.BaseType;
+            }
             type = GetRawType(type);
             if (type.IsGenericParameter) return;
             refTypes.Add(type);
@@ -610,13 +617,6 @@ namespace Puerts.Editor
                     {
                         AddRefType(refTypes, pinfo.ParameterType);
                     }
-                }
-
-                var baseType = type.BaseType;
-                while (baseType != null)
-                {
-                    AddRefType(refTypes, baseType);
-                    baseType = baseType.BaseType;
                 }
             }
 
