615bcb2b99d8fbc07decae46af10e57dd2e4b2c3
johnche <johnche@tencent.com>
2020-09-01 14:21:40 +0800

    非unity版本的实例，以及ut用例

---
+++ unity/general/Src/Helloworld.cs
@@ -0,0 +1,39 @@
+/*
+* Tencent is pleased to support the open source community by making Puerts available.
+* Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+* Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
+* This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
+*/
+
+using System.IO;
+using Puerts;
+
+public class TxtLoader : ILoader
+{
+    private string root = "../../Assets/Puerts/Src/Resources";
+
+    public bool FileExists(string filepath)
+    {
+        return File.Exists(Path.Combine(root, filepath + ".txt"));
+    }
+
+    public string ReadFile(string filepath, out string debugpath)
+    {
+        debugpath = Path.Combine(root, filepath);
+        return File.ReadAllText(debugpath + ".txt");
+    }
+}
+
+public class PuertsTest
+{
+    public static void Main()
+    {
+        var jsEnv = new JsEnv(new TxtLoader());
+        jsEnv.Eval(@"
+                const CS = require('csharp');
+                CS.System.Console.WriteLine('hello world');
+            ");
+
+        jsEnv.Dispose();
+    }
+}
---
+++ unity/general/Src/UnitTest/UnitTest.cs
@@ -0,0 +1,59 @@
+/*
+* Tencent is pleased to support the open source community by making Puerts available.
+* Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+* Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
+* This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
+*/
+
+using NUnit.Framework;
+using System.IO;
+
+namespace Puerts.UnitTest
+{
+    public class TxtLoader : ILoader
+    {
+        private string root = "../../Assets/Puerts/Src/Resources";
+
+        public bool FileExists(string filepath)
+        {
+            return File.Exists(Path.Combine(root, filepath + ".txt"));
+        }
+
+        public string ReadFile(string filepath, out string debugpath)
+        {
+            debugpath = Path.Combine(root, filepath);
+            return File.ReadAllText(debugpath + ".txt");
+        }
+    }
+
+    [TestFixture]
+    public class PuertsTest
+    {
+        [OneTimeSetUp]
+        public static void Init()
+        {
+        }
+
+        [Test]
+        public void BaseTest()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let obj = new CS.Puerts.UnitTest.DerivedClass();
+                obj.Id(100);
+            ");
+
+            jsEnv.Dispose();
+
+            Assert.AreEqual(100, ret);
+        }
+
+        [Test]
+        public void FailTest()
+        {
+            Assert.AreEqual(100, 1);
+        }
+    }
+}
