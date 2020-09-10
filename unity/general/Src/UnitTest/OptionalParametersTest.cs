f3bf9edfc2dfed570162b039aec42c813315f181
纪华裕 <8042833@qq.com>
2020-09-10 13:56:15 +0800

    修复默认参数更新引起的 bug (#31)
    * C# 缺省参数 d.ts 生成
    
    * C# 缺省参数 C# 绑定代码生成
    
    * C# 缺省参数反射调用
    
    * 添加 可选参数 单元测试
    
    * 修复默认参数更新引起的 bug
    
    * 解决冲突
    
    * 继续修复默认参数Bug，添加单元测试。
    
    * 继续修复默认参数Bug
    
    * 优化代码

--- unity/general/Src/UnitTest/OptionalParametersTest.cs
+++ unity/general/Src/UnitTest/OptionalParametersTest.cs
@@ -13,6 +13,36 @@ namespace Puerts.UnitTest
         {
             return j * 10 + k;
         }
+        
+        public int Test2(string i)
+        {
+            return 0;
+        }
+        
+        public int Test2(string i, int j)
+        {
+            return j;
+        }
+        
+        public int Test2(string i, int j ,params bool[] k)
+        {
+            return -1;
+        }
+        
+        public int Test3(string i,int b)
+        {
+            return 0;
+        }
+        
+        public int Test4(string i,int b,int c = 0, int d = 1)
+        {
+            return 0;
+        }
+        
+        public int Test5(string i, int j ,params bool[] k)
+        {
+            return -1;
+        }
     }
 
     [TestFixture]
@@ -70,7 +100,127 @@ namespace Puerts.UnitTest
             Assert.AreEqual(666, ret);
             jsEnv.Dispose();
         }
+        [Test]
+        public void WarpTest5()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test2('1',100);
+            ");
+            Assert.AreEqual(100, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void WarpTest6()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test2('1');
+            ");
+            Assert.AreEqual(0, ret);
+            jsEnv.Dispose();
+        }
+        [Test]
+        public void WarpTest7()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
 
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test5('1', 1, false,false,false);
+            ");
+            Assert.AreEqual(-1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void WarpTest8()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test5('1', 1, false);
+            ");
+            Assert.AreEqual(-1, ret);
+            jsEnv.Dispose();
+        }
+        
+        //行为不一致
+        /*[Test]
+        public void WarpTest9()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test3('1');}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(1, ret);
+            jsEnv.Dispose();
+        }*/
+        
+        [Test]
+        public void WarpTest10()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test3('1',1);}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(0, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void WarpTest11()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test4('1');}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void WarpTest12()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            PuertsStaticWrap.AutoStaticCodeRegister.Register(jsEnv);
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test4('1',1);}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(0, ret);
+            jsEnv.Dispose();
+        }
+        
         [Test]
         public void ReflectTest1()
         {
@@ -117,6 +267,118 @@ namespace Puerts.UnitTest
                 temp.Test(6,6,6);
             ");
             Assert.AreEqual(666, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest5()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test2('1',100);
+            ");
+            Assert.AreEqual(100, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest6()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test2('1');
+            ");
+            Assert.AreEqual(0, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest7()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test5('1', 1, false,false,false);
+            ");
+            Assert.AreEqual(-1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest8()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                temp.Test5('1', 1, false);
+            ");
+            Assert.AreEqual(-1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest9()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test3('1');}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest10()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test3('1',1);}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(0, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest11()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test4('1');}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(1, ret);
+            jsEnv.Dispose();
+        }
+        
+        [Test]
+        public void ReflectTest12()
+        {
+            var jsEnv = new JsEnv(new TxtLoader());
+            int ret = jsEnv.Eval<int>(@"
+                const CS = require('csharp');
+                let temp = new CS.Puerts.UnitTest.OptionalParametersClass();
+                let ret = 0;                
+                try{temp.Test4('1',1);}catch(e){ret = 1;}
+                ret;
+            ");
+            Assert.AreEqual(0, ret);
             jsEnv.Dispose();
         }
     }
--- unity/general/Src/UnitTest/warp/Puerts_UnitTest_OptionalParametersClass_Wrap.cs
+++ unity/general/Src/UnitTest/warp/Puerts_UnitTest_OptionalParametersClass_Wrap.cs
@@ -158,6 +158,235 @@ namespace PuertsStaticWrap
             }
         }
         
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void M_Test2(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
+                
+                if (paramLen == 1)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var result = obj.Test2(Arg0);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                if (paramLen == 2)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
+                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var result = obj.Test2(Arg0,Arg1);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                if (paramLen == 3)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
+                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
+                        && argHelper2.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.NativeObject, typeof(bool), false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var Arg2 = argHelper2.GetParams<bool>(info, 2, paramLen);
+                        var result = obj.Test2(Arg0,Arg1,Arg2);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test2");
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void M_Test3(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
+                
+                
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    
+                    
+                    
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var result = obj.Test3(Arg0,Arg1);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        
+                    }
+                }
+                
+                
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void M_Test4(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
+                
+                if (paramLen == 4)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
+                    var argHelper3 = new Puerts.ArgumentHelper((int)data, isolate, info, 3);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
+                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
+                        && argHelper2.IsMatch(Puerts.JsValueType.Number, null, false, false)
+                        && argHelper3.IsMatch(Puerts.JsValueType.Number, null, false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var Arg2 = argHelper2.GetInt32(false);
+                        var Arg3 = argHelper3.GetInt32(false);
+                        var result = obj.Test4(Arg0,Arg1,Arg2,Arg3);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                if (paramLen == 3)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
+                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
+                        && argHelper2.IsMatch(Puerts.JsValueType.Number, null, false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var Arg2 = argHelper2.GetInt32(false);
+                        var result = obj.Test4(Arg0,Arg1,Arg2);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                if (paramLen == 2)
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    
+                    
+                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
+                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false))
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var result = obj.Test4(Arg0,Arg1);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        return;
+                    }
+                }
+                
+                Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test4");
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void M_Test5(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
+                
+                
+                {
+                    
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
+                    
+                    
+                    
+                    {
+                        
+                        var Arg0 = argHelper0.GetString(false);
+                        var Arg1 = argHelper1.GetInt32(false);
+                        var Arg2 = argHelper2.GetParams<bool>(info, 2, paramLen);
+                        var result = obj.Test5(Arg0,Arg1,Arg2);
+                        
+                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
+                        
+                    }
+                }
+                
+                
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        
         
         
         
@@ -171,6 +400,10 @@ namespace PuertsStaticWrap
                 Methods = new System.Collections.Generic.Dictionary<Puerts.MethodKey, Puerts.V8FunctionCallback>()
                 {
                     { new Puerts.MethodKey {Name = "Test", IsStatic = false},  M_Test },
+                    { new Puerts.MethodKey {Name = "Test2", IsStatic = false},  M_Test2 },
+                    { new Puerts.MethodKey {Name = "Test3", IsStatic = false},  M_Test3 },
+                    { new Puerts.MethodKey {Name = "Test4", IsStatic = false},  M_Test4 },
+                    { new Puerts.MethodKey {Name = "Test5", IsStatic = false},  M_Test5 },
                     
                 },
                 Properties = new System.Collections.Generic.Dictionary<string, Puerts.PropertyRegisterInfo>()
