f2b0e0c3db79deaa93f6c630350086ff2722be4b
johnche <johnche@tencent.com>
2020-11-04 10:11:11 +0800

    [unreal]android编译报错

--- unreal/Puerts/Source/JsEnv/Private/PromiseRejectCallback.hpp
+++ unreal/Puerts/Source/JsEnv/Private/PromiseRejectCallback.hpp
@@ -1,56 +1,58 @@
 namespace puerts
 {
-    template<typename T>
-    void PromiseRejectCallback(v8::PromiseRejectMessage Message)
+template <typename T> inline void __USE(T&&) {}
+
+template<typename T>
+void PromiseRejectCallback(v8::PromiseRejectMessage Message)
+{
+    auto Promise = Message.GetPromise();
+    auto Isolate = Promise->GetIsolate();
+    auto Event = Message.GetEvent();
+
+    auto JsEngine = T::Get(Isolate);
+    auto Callback = JsEngine->JsPromiseRejectCallback.Get(Isolate);
+    if (Callback.IsEmpty())
     {
-        auto Promise = Message.GetPromise();
-        auto Isolate = Promise->GetIsolate();
-        auto Event = Message.GetEvent();
-
-        auto JsEngine = T::Get(Isolate);
-        auto Callback = JsEngine->JsPromiseRejectCallback.Get(Isolate);
-        if (Callback.IsEmpty())
-        {
-            return;
-        }
-
-        v8::Local<v8::Value> Value;
-
-        if (Event == v8::kPromiseRejectWithNoHandler)
-        {
-            Value = Message.GetValue();
-        }
-        else if (Event == v8::kPromiseHandlerAddedAfterReject)
-        {
-            Value = v8::Undefined(Isolate);
-        }
-        else if (Event == v8::kPromiseResolveAfterResolved)
-        {
-            Value = Message.GetValue();
-        }
-        else if (Event == v8::kPromiseRejectAfterResolved)
-        {
-            Value = Message.GetValue();
-        }
-        else
-        {
-            return;
-        }
-
-        if (Value.IsEmpty())
-        {
-            Value = v8::Undefined(Isolate);
-        }
-
-        v8::Local<v8::Value> Args[] = { v8::Number::New(Isolate, Event), Promise, Value };
-        Callback->Call(Isolate->GetCurrentContext(), v8::Undefined(Isolate), 3, Args);
+        return;
     }
 
-    template<typename T>
-    void SetPromiseRejectCallback(const v8::FunctionCallbackInfo<v8::Value>& Args)
+    v8::Local<v8::Value> Value;
+
+    if (Event == v8::kPromiseRejectWithNoHandler)
     {
-        auto Isolate = Args.GetIsolate();
-        auto JsEngine = T::Get(Isolate);
-        JsEngine->JsPromiseRejectCallback.Reset(Isolate, Args[0].As<v8::Function>());
+        Value = Message.GetValue();
     }
+    else if (Event == v8::kPromiseHandlerAddedAfterReject)
+    {
+        Value = v8::Undefined(Isolate);
+    }
+    else if (Event == v8::kPromiseResolveAfterResolved)
+    {
+        Value = Message.GetValue();
+    }
+    else if (Event == v8::kPromiseRejectAfterResolved)
+    {
+        Value = Message.GetValue();
+    }
+    else
+    {
+        return;
+    }
+
+    if (Value.IsEmpty())
+    {
+        Value = v8::Undefined(Isolate);
+    }
+
+    v8::Local<v8::Value> Args[] = { v8::Number::New(Isolate, Event), Promise, Value };
+    __USE(Callback->Call(Isolate->GetCurrentContext(), v8::Undefined(Isolate), 3, Args));
+}
+
+template<typename T>
+void SetPromiseRejectCallback(const v8::FunctionCallbackInfo<v8::Value>& Args)
+{
+    auto Isolate = Args.GetIsolate();
+    auto JsEngine = T::Get(Isolate);
+    JsEngine->JsPromiseRejectCallback.Reset(Isolate, Args[0].As<v8::Function>());
+}
 }
