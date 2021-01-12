956b89e48d562e90619d90ac5bbdc4edc21da4ff
johnche <johnche@tencent.com>
2021-01-12 15:14:39 +0800

    [unreal]配置项名称调整

--- unreal/Puerts/Source/Puerts/Private/PuertsSetting.h
+++ unreal/Puerts/Source/Puerts/Private/PuertsSetting.h
@@ -14,15 +14,15 @@ class UPuertsSetting : public UObject
 {
 	GENERATED_BODY()
 public:
-    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "DebugEnable", defaultValue = false))
+    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "Puerts Module Enable", defaultValue = false))
     bool Enable = false;
 
-    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "DebugEnable", defaultValue = false))
+    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "Debug Enable", defaultValue = false))
     bool DebugEnable = false;
 
-    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "DebugPort", defaultValue = 8080))
+    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "Debug Port", defaultValue = 8080))
     int32 DebugPort = 8080;
 
-    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "WaitDebugger", defaultValue = false))
+    UPROPERTY(config, EditAnywhere, Category = "Setting", meta = (DisplayName = "Wait Debugger", defaultValue = false))
     bool WaitDebugger = false;
 };
