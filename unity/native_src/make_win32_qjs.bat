09a5150e3a644aa8cf4c25272361b018702fb756
johnche <johnche@tencent.com>
2021-02-27 17:39:35 +0800

    quickjs版本拷贝dll，并且调整action workflow的文件名

--- unity/native_src/make_win32_qjs.bat
+++ unity/native_src/make_win32_qjs.bat
@@ -2,4 +2,5 @@ mkdir build32qjs & pushd build32qjs
 cmake -DJS_ENGINE=quickjs -G "Visual Studio 16 2019" -A Win32 ..
 popd
 cmake --build build32qjs --config Release
-pause
+md ..\Assets\Plugins\x86
+copy build32qjs\Release\puerts.dll ..\Assets\Plugins\x86
--- unity/native_src/make_win64_qjs.bat
+++ unity/native_src/make_win64_qjs.bat
@@ -2,4 +2,5 @@ mkdir build64qjs & pushd build64qjs
 cmake -DJS_ENGINE=quickjs -G "Visual Studio 16 2019" -A x64 ..
 popd
 cmake --build build64qjs --config Release
-pause
+md ..\Assets\Plugins\x86_64
+copy build64qjs\Release\puerts.dll ..\Assets\Plugins\x86_64
