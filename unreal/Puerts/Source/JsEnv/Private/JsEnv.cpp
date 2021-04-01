f5304394cdd9386e734266114958ef46d30a6e30
johnche <johnche@tencent.com>
2021-04-01 10:39:27 +0800

    [unreal]添加ue重载new导致的问题的处理

--- unreal/Puerts/Source/JsEnv/Private/JsEnv.cpp
+++ unreal/Puerts/Source/JsEnv/Private/JsEnv.cpp
@@ -711,6 +711,12 @@ FJsEnvImpl::FJsEnvImpl(std::unique_ptr<IJSModuleLoader> InModuleLoader, std::sha
     ReloadJs.Reset(Isolate, Puerts->Get(Context, FV8Utils::ToV8String(Isolate, "__reload")).ToLocalChecked().As<v8::Function>());
 
     DelegateProxysCheckerHandler = FTicker::GetCoreTicker().AddTicker(TBaseDelegate<bool, float>::CreateRaw(this, &FJsEnvImpl::CheckDelegateProxys), 1);
+
+    int * Dummy = new (std::nothrow) int[0];
+    if (!Dummy)
+    {
+        Logger->Warn("new (std::nothrow) int[0] return nullptr");
+    }
 }
 
 // #lizard forgives
