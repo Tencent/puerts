596c4bfa86c4f9654ecb2f0555fdcaf844b8b187
johnche <johnche@tencent.com>
2021-04-06 16:52:37 +0800

    [unreal]将react-umg模块剥离到puerts之外

---
+++ unreal/Puerts/Source/DeclarationGenerator/Private/CodeGenerator.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "CodeGenerator.h"
+
+// Add default functionality here for any ICodeGenerator functions that are not pure virtual.
