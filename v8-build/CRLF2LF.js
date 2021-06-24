19b5dadcb8ca1f69e6bdb4e1ef99c3b2c7bdbe91
xosuperpig <xosuperpig>
2021-06-24 16:45:19 +0800

    CRLF

--- v8-build/CRLF2LF.js
+++ v8-build/CRLF2LF.js
@@ -1,4 +1,4 @@
 const fs = require('fs');
 var file = process.argv[2];
 
-fs.writeFileSync(file, fs.readFileSync(file, {encoding: 'utf-8'}).toString().replace(/\\r\\n/g, /\\n/g));
+fs.writeFileSync(file, fs.readFileSync(file, {encoding: 'utf-8'}).toString().replace(/\r\n/g, '\n'));
