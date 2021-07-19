4d3425ba383cfbfa388e87de6aebf3f5ac7623c7
zombieyang <zombieyang@tencent.com>
2021-07-19 19:26:04 +0800

    linux32

---
+++ unity/native_src_ourv8/make_linux64.sh
@@ -0,0 +1,6 @@
+mkdir -p build_linux64 && cd build_linux64
+cmake ..
+cd ..
+cmake --build build_linux64 --config Release
+mkdir -p ../Assets/Plugins/x86_64
+cp build_linux64/libpuerts.so ../Assets/Plugins/x86_64
