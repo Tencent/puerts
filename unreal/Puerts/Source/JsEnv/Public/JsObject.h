e6cf89eb51eb189ab4b0a5e958d4f8c4bdc271a7
johnche <johnche@tencent.com>
2021-07-29 19:13:17 +0800

    [unreal]支持传js对象到c++

---
+++ unreal/Puerts/Source/JsEnv/Public/JsObject.h
@@ -0,0 +1,112 @@
+/*
+* Tencent is pleased to support the open source community by making Puerts available.
+* Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+* Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms.
+* This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
+*/
+
+#pragma once
+
+#pragma warning(push, 0) 
+#include "v8.h"
+#pragma warning(pop)
+
+#include "Binding.hpp"
+
+#include "CoreMinimal.h"
+#include "JsObject.generated.h"
+
+USTRUCT(BlueprintType)
+struct FJsObject
+{
+public:
+	GENERATED_USTRUCT_BODY()
+
+	FJsObject():Isolate(nullptr){}
+
+	FJsObject(const FJsObject & InOther)
+	{
+		Isolate = InOther.Isolate;
+		GContext.Reset(Isolate, InOther.GContext.Get(Isolate));
+		GObject.Reset(Isolate, InOther.GObject.Get(Isolate));
+	}
+	
+	FJsObject(v8::Local<v8::Context> InContext, v8::Local<v8::Object> InObject)
+		: Isolate(InContext->GetIsolate()),
+	      GContext(InContext->GetIsolate(), InContext),
+	      GObject(InContext->GetIsolate(), InObject)
+	{}
+
+	FJsObject & operator =(const FJsObject & InOther)
+	{
+		Isolate = InOther.Isolate;
+		GContext.Reset(Isolate, InOther.GContext.Get(Isolate));
+		GObject.Reset(Isolate, InOther.GObject.Get(Isolate));
+		return *this;
+	}
+
+	template<typename T>
+	T Get(const char* Key)
+	{
+		v8::Isolate::Scope IsolateScope(Isolate);
+		v8::HandleScope HandleScope(Isolate);
+		auto Context = GContext.Get(Isolate);
+		v8::Context::Scope ContextScope(Context);
+		auto Object = GObject.Get(Isolate);
+		
+		auto MaybeValue = Object->Get(Context, puerts::converter::Converter<const char*>::toScript(Context, Key));
+		v8::Local<v8::Value> Val;
+		if (MaybeValue.ToLocal(&Val))
+		{
+			return puerts::converter::Converter<T>::toCpp(Context, Val);
+		}
+		return {};
+	}
+
+	template<typename T>
+	void Set(const char* Key, T Val)
+	{
+		v8::Isolate::Scope IsolateScope(Isolate);
+		v8::HandleScope HandleScope(Isolate);
+		auto Context = GContext.Get(Isolate);
+		v8::Context::Scope ContextScope(Context);
+		auto Object = GObject.Get(Isolate);
+		
+		auto _UnUsed = Object->Set(Context, puerts::converter::Converter<const char*>::toScript(Context, Key),
+			puerts::converter::Converter<T>::toScript(Context, Val));
+	}
+
+public:
+	v8::Isolate *Isolate;
+	v8::Global<v8::Context> GContext;
+	v8::Global<v8::Object> GObject;
+};
+
+namespace puerts
+{
+template<>
+struct ScriptTypeName<FJsObject> {
+    static constexpr const char * value = "object";
+};
+
+namespace converter
+{
+template <>
+struct Converter<FJsObject> {
+	static v8::Local<v8::Value> toScript(v8::Local<v8::Context> context, FJsObject value)
+	{
+		return value.GObject.Get(context->GetIsolate());
+	}
+
+	static FJsObject toCpp(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+	{
+		return FJsObject(context, value.As<v8::Object>());
+	}
+
+	static bool accept(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+	{
+		return value->IsObject();
+	}
+};
+}	
+}
