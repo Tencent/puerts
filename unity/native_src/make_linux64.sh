dfb256d67ee3a284b110668a1f64944d1968c124
zombieyang <zombieyang@tencent.com>
2021-08-16 14:19:43 +0800

    resolve conflict

--- unity/native_src/make_linux64.sh
+++ unity/native_src/make_linux64.sh
@@ -1,15 +1,6 @@
 mkdir -p build_linux64 && cd build_linux64
-<<<<<<< HEAD
-cmake ..
-cd ..
-cmake --build build_linux64 --config Release
-mkdir -p ../Assets/Plugins/x86_64
-cp build_linux64/libpuerts.so ../Assets/Plugins/x86_64
-=======
 cmake ../
 cd ..
 cmake --build build_linux64 --config Release
 mkdir -p ../Assets/Plugins/x86_64/
 cp build_linux64/libpuerts.so ../Assets/Plugins/x86_64/libpuerts.so 
-
->>>>>>> 9195866a522b97bedfae42b1c43784cd7f8acc54
