6cca2c8fa0fd66b41b8116a9b7c3f0f03b01a6b9
zombieyang <zombieyang@tencent.com>
2021-08-18 15:47:42 +0800

    HandleScope optimize

--- unity/native_src/Src/JSEngine.cpp
+++ unity/native_src/Src/JSEngine.cpp
@@ -274,10 +274,7 @@ namespace puerts
     static void CSharpFunctionCallbackWrap(const v8::FunctionCallbackInfo<v8::Value>& Info)
     {
         v8::Isolate* Isolate = Info.GetIsolate();
-        v8::Isolate::Scope IsolateScope(Isolate);
-        v8::HandleScope HandleScope(Isolate);
         v8::Local<v8::Context> Context = Isolate->GetCurrentContext();
-        v8::Context::Scope ContextScope(Context);
 
         FCallbackInfo* CallbackInfo = reinterpret_cast<FCallbackInfo*>((v8::Local<v8::External>::Cast(Info.Data()))->Value());
 
