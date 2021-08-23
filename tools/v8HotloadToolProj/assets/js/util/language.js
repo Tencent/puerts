0129693441b7b0e57eefd343e99ff16cac91d95f
throw-out <45587825+throw-out@users.noreply.github.com>
2021-08-23 09:51:54 +0800

    Delete tools/v8HotloadToolProj/assets/js directory

--- tools/v8HotloadToolProj/assets/js/util/language.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.Language = void 0;
-class Language {
-}
-exports.Language = Language;
-//# sourceMappingURL=language.js.map
