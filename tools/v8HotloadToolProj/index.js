7ab20e35a5fb7fb5ed2119431f53fac0c9fc27fe
throw-out <45587825+throw-out@users.noreply.github.com>
2021-08-23 11:58:33 +0800

    v8 hotload tool

---
+++ tools/v8HotloadToolProj/index.js
@@ -0,0 +1,1 @@
+require("./assets/js/app");
---
+++ tools/v8HotloadToolProj/src/renderer-process/index.ts
@@ -0,0 +1,7 @@
+import Vue from "vue";
+import * as ElementUI from "element-ui";
+import "element-ui/lib/theme-chalk/index.css";
+Vue.use(ElementUI);
+
+require("./command");
+require("./sections/window");
