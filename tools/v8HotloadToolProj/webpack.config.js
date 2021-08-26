bae02377cf9204a5f1f2a92c4ed33fb4d1060cd8
throw-out <45587825+throw-out@users.noreply.github.com>
2021-08-26 09:25:08 +0800

    v8 hotload tool (#409)
    * v8 hotload tool
    
    * 修复scriptSource格式化, 添加.gitignore文件

---
+++ tools/v8HotloadToolProj/webpack.config.js
@@ -0,0 +1,35 @@
+const path = require("path");
+module.exports = {
+    mode: "production",
+    entry: {
+        index: path.resolve(__dirname, "./src/renderer-process/index.ts"),
+    },
+    module: {
+        rules: [{
+            test: /\.tsx?$/,
+            use: "ts-loader",
+            exclude: /node_modules/,
+        },
+        {
+            test: /\.css$/,
+            use: ["style-loader", "css-loader"]
+        }]
+    },
+    resolve: {
+        extensions: [".tsx", ".ts", ".js", ".vue", ".json"],
+        alias: {
+            vue$: "vue/dist/vue.esm.js", //使用vue runtime compuler
+            "@": path.resolve("src")
+        }
+    },
+    output: {
+        filename: "[name].bundle.js",
+        path: path.resolve(__dirname, "./assets/html"),
+        publicPath: "./",
+    },
+    externals: {
+        "fs": "commonjs2 fs",
+        "path": "commonjs2 path",
+        "electron": "commonjs2 electron",
+    }
+};
