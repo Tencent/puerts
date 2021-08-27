591757abd3dbc607f05fb08281abff4ed1fce1e5
zombieyang(杨浩) <zombieyang@tencent.com>
2021-08-27 17:00:00 +0800

    trigger ci

--- unity/native_src/Src/JSEngine.cpp
+++ unity/native_src/Src/JSEngine.cpp
@@ -4,7 +4,6 @@
 * Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms.
 * This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
 */
-
 #include "JSEngine.h"
 #include "V8Utils.h"
 #include "Log.h"
