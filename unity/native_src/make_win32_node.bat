e8665ad9a9fcc8645a9bdb59698ed32061d6310d
zombieyang(杨浩) <zombieyang@tencent.com>
2021-08-27 16:31:11 +0800

    windows build fix

--- unity/native_src/make_win32_node.bat
+++
@@ -1,7 +0,0 @@
-mkdir build32nodejs & pushd build32qjs
-cmake -DJS_ENGINE=nodejs -G "Visual Studio 16 2019" -A Win32 ..
-popd
-cmake --build build32nodejs --config Release
-md ..\Assets\Plugins\x86
-copy build32nodejs\Release\puerts.dll ..\Assets\Plugins\x86
-copy quickjs\Lib\Win32\*.dll ..\Assets\Plugins\x86
