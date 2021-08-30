e21d2651f94deb4c4fc11f9e7ef62e0a794d719a
zombieyang <zombieyang@tencent.com>
2021-08-30 18:12:55 +0800

    remove oursv8

--- unity/native_src_ourv8/make_ios_simulator.sh
+++
@@ -1,5 +0,0 @@
-mkdir -p build_ios_simulator && cd build_ios_simulator
-cmake -DCMAKE_TOOLCHAIN_FILE=../cmake/ios.toolchain.cmake -DPLATFORM=SIMULATOR64 -GXcode ../
-cd ..
-cmake --build build_ios_simulator --config Release
-
