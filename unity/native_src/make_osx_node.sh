9f174b7a42c4b678a232d415d1155a3e1d0ae032
僵尸浩 <348063288@qq.com>
2021-08-31 15:52:13 +0800

    修改cmake rpath逻辑，防止找不到node的dylib (#425)

--- unity/native_src/make_osx_node.sh
+++ unity/native_src/make_osx_node.sh
@@ -4,15 +4,16 @@ then
     cmake -DJS_ENGINE=nodejs -DFOR_UT=1 -GXcode ../
     cd ..
     cmake --build build_osx_nodejs --config Debug
+    cp -r build_osx_nodejs/Debug/libpuerts.dylib ../general/Bin/
+
 else
     cmake -DJS_ENGINE=nodejs -GXcode ../
     cd ..
     cmake --build build_osx_nodejs --config Release
-fi
-mkdir -p ../Assets/Plugins/
-cp -r build_osx_nodejs/Release/puerts.bundle ../Assets/Plugins/
-cp -r nodejs/lib/macOS/libnode.83.dylib ../Assets/Plugins/
-if [ "$1" == "-ut" ]
-then
-    cp -r build_osx_nodejs/Release/libpuerts.dylib ../general/Bin/
+    cmake --install build_osx_nodejs --prefix "$(pwd)/build_osx_nodejs"
+    mv build_osx_nodejs/bin/libpuerts.dylib build_osx_nodejs/bin/libpuerts.bundle
+    cp -r nodejs/lib/macOS/libnode.83.dylib build_osx_nodejs/bin/
+
+    mkdir -p ../Assets/Plugins/macOS
+    cp -r build_osx_nodejs/bin/* ../Assets/Plugins/macOS/
 fi
