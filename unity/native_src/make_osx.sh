8ffeaaaa439c2fdb314ebfac4cf6053bb76a4484
zombieyang <zombieyang@tencent.com>
2021-08-31 15:34:20 +0800

    osx rpath

--- unity/native_src/make_osx.sh
+++ unity/native_src/make_osx.sh
@@ -1,17 +1,16 @@
 mkdir -p build_osx && cd build_osx
 if [ "$1" == "-ut" ]
 then
-    cmake -DFOR_UT=ON -DWITH_NODE=ON -GXcode ../
+    cmake -DFOR_UT=ON -GXcode ../
     cd ..
     cmake --build build_osx --config Debug
+    cp -r build_osx/Debug/libpuerts.dylib ../general/Bin/
 else
-    cmake -GXcode -DWITH_NODE=ON ../
+    cmake -GXcode ../
     cd ..
     cmake --build build_osx --config Release
-fi
-mkdir -p ../Assets/Plugins/
-cp -r build_osx/Release/puerts.bundle ../Assets/Plugins/
-if [ "$1" == "-ut" ]
-then
-    cp -r build_osx/Release/libpuerts.dylib ../general/Bin/
+    cmake --install build_osx --prefix "$(pwd)/build_osx"
+    mv build_osx/bin/libpuerts.dylib build_osx/bin/libpuerts.bundle
+    mkdir -p ../Assets/Plugins/macOS
+    cp -r build_osx/bin/* ../Assets/Plugins/macOS/
 fi
