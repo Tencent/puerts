fbd7bd603554d47dd5497fc1ee6acd2fa71b6260
zombieyang <zombieyang@tencent.com>
2021-09-06 01:21:35 +0800

    update libversion for node

--- unity/Assets/Puerts/Src/JsEnv.cs
+++ unity/Assets/Puerts/Src/JsEnv.cs
@@ -66,7 +66,7 @@ namespace Puerts
 
         public JsEnv(ILoader loader, int debugPort, JsEnvMode mode, IntPtr externalRuntime, IntPtr externalContext)
         {
-            const int libVersionExpect = 11;
+            const int libVersionExpect = 12;
             int libVersion = PuertsDLL.GetLibVersion();
             if (libVersion != libVersionExpect)
             {
