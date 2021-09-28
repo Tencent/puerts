79b78546ff857b4d49d04e54beba0dac2e27391c
johnche <johnche@tencent.com>
2021-09-28 16:52:19 +0800

    [unreal]启动速度优化，将ts工程的md5保存，下次启动时，如果发现md5不变，跳过js文件以及蓝图的检查，这在一些大项目可以从几分钟优化为几秒

--- unreal/Puerts/Content/JavaScript/PuertsEditor/CodeAnalyze.js
+++ unreal/Puerts/Content/JavaScript/PuertsEditor/CodeAnalyze.js
@@ -1145,11 +1145,13 @@ function readAndParseConfigFile(configFilePath) {
 function watch(configFilePath) {
     let { fileNames, options } = readAndParseConfigFile(configFilePath);
     console.log("start watch..", JSON.stringify({ fileNames: fileNames, options: options }));
+    const versionsFilePath = getDirectoryPath(configFilePath) + "/ts_file_versions_info.json";
     const fileVersions = {};
-    // initialize the list of files
+    let beginTime = new Date().getTime();
     fileNames.forEach(fileName => {
-        fileVersions[fileName] = { version: "" };
+        fileVersions[fileName] = { version: UE.FileSystemOperation.FileMD5Hash(fileName) };
     });
+    console.log("calc md5 using " + (new Date().getTime() - beginTime) + "ms");
     function getDefaultLibLocation() {
         return getDirectoryPath(normalizePath(customSystem.getExecutingFilePath()));
     }
@@ -1196,7 +1198,7 @@ function watch(configFilePath) {
             service = ts.createLanguageService(servicesHost, ts.createDocumentRegistry());
         }
     }
-    let beginTime = new Date().getTime();
+    beginTime = new Date().getTime();
     let program = getProgramFromService();
     console.log("full compile using " + (new Date().getTime() - beginTime) + "ms");
     let diagnostics = ts.getPreEmitDiagnostics(program);
@@ -1204,19 +1206,33 @@ function watch(configFilePath) {
         logErrors(diagnostics);
     }
     else {
+        let restoredFileVersions = {};
+        if (customSystem.fileExists(versionsFilePath)) {
+            try {
+                restoredFileVersions = JSON.parse(customSystem.readFile(versionsFilePath));
+                console.log("restore versions from ", versionsFilePath);
+            }
+            catch { }
+        }
         fileNames.forEach(fileName => {
-            onSourceFileAddOrChange(fileName, false, program, true, false);
+            if (!(fileName in restoredFileVersions) || restoredFileVersions[fileName].version != fileVersions[fileName].version) {
+                onSourceFileAddOrChange(fileName, false, program, true, false);
+            }
         });
         fileNames.forEach(fileName => {
-            onSourceFileAddOrChange(fileName, false, program, false);
+            if (!(fileName in restoredFileVersions) || restoredFileVersions[fileName].version != fileVersions[fileName].version) {
+                onSourceFileAddOrChange(fileName, false, program, false);
+            }
         });
     }
     var dirWatcher = new UE.PEDirectoryWatcher();
     global.__dirWatcher = dirWatcher; //防止被释放?
     dirWatcher.OnChanged.Add((added, modified, removed) => {
         setTimeout(() => {
+            var changed = false;
             if (added.Num() > 0) {
                 onFileAdded();
+                changed = true;
             }
             if (modified.Num() > 0) {
                 for (var i = 0; i < modified.Num(); i++) {
@@ -1230,9 +1246,14 @@ function watch(configFilePath) {
                             console.log(`${fileName} md5 from ${fileVersions[fileName].version} to ${md5}`);
                             fileVersions[fileName].version = md5;
                             onSourceFileAddOrChange(fileName, true);
+                            changed = true;
                         }
                     }
                 }
+            }
+            if (changed) {
+                console.log("versions saved to " + versionsFilePath);
+                UE.FileSystemOperation.WriteFile(versionsFilePath, JSON.stringify(fileVersions, null, 4));
             }
         }, 100); //延时100毫秒，防止因为读冲突而文件读取失败
     });
