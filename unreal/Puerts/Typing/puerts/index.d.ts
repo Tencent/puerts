e774dc2dccd41db2a4bc7e05eb71ed1cefd0e7bc
liangcheng <liangcheng@kingsoft.com>
2021-10-25 19:34:53 +0800

    chore: 增加变量声明 (#4)
    * chore: 增加变量声明
    
    * chore: 撤销错误提交
    
    * chore: 撤销修改
    
    * chore: 撤销无用修改

--- unreal/Puerts/Typing/puerts/index.d.ts
+++ unreal/Puerts/Typing/puerts/index.d.ts
@@ -62,3 +62,9 @@ declare module "puerts" {
 }
 
 declare function require(name: string): any;
+
+// --> modified by kg begin
+// liangcheng: 因为require时puertr调用时传入了__filename以及__dirname，所以要使用他们得加个声明
+declare const __filename: string;
+declare const __dirname: string;
+// --< end
