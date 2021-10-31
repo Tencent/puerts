f55169bc7a442e1a81c8e235ce51d8e458514e44
zombieyang <zombieyang@tencent.com>
2021-10-31 13:55:11 +0800

    .

--- unity/general/Src/UnitTest/warp/Puerts_UnitTest_OptionalParametersClass_Wrap.cs
+++ unity/general/Src/UnitTest/warp/Puerts_UnitTest_OptionalParametersClass_Wrap.cs
@@ -1,6 +1,4 @@
-﻿
 using System;
-
 
 namespace PuertsStaticWrap
 {
@@ -26,7 +24,6 @@ namespace PuertsStaticWrap
                         
                         return Puerts.Utils.GetObjectPtr((int)data, typeof(Puerts.UnitTest.OptionalParametersClass), result);
                     }
-                    
                 }
                 
                 
@@ -37,7 +34,6 @@ namespace PuertsStaticWrap
             }
             return IntPtr.Zero;
         }
-        
         
         [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
         private static void M_Test(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
@@ -65,10 +61,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0,Arg1,Arg2);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                     if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
                         && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
                         && argHelper2.IsMatch(Puerts.JsValueType.Number, null, false, false))
@@ -80,10 +74,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0,Arg1,Arg2);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 2)
@@ -102,10 +94,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0,Arg1);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                     if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
                         && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false))
                     {
@@ -115,10 +105,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0,Arg1);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 1)
@@ -134,10 +122,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                     if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false))
                     {
                         
@@ -145,10 +131,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test(Arg0);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 0)
@@ -162,10 +146,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test();
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test");
@@ -175,8 +157,6 @@ namespace PuertsStaticWrap
                 Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
             }
         }
-        
-        
         
         [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
         private static void M_Test2(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
@@ -198,10 +178,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test2(Arg0);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 2)
@@ -220,13 +198,11 @@ namespace PuertsStaticWrap
                         var result = obj.Test2(Arg0,Arg1);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
-                if (paramLen >= 2)
+                if (paramLen == 3)
                 {
                     
                     var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
@@ -236,7 +212,7 @@ namespace PuertsStaticWrap
                     
                     if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
                         && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
-                        && argHelper2.IsMatchParams(Puerts.JsValueType.Boolean, null, 2, paramLen))
+                        && argHelper2.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.NativeObject, typeof(bool), false, false))
                     {
                         
                         var Arg0 = argHelper0.GetString(false);
@@ -245,10 +221,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test2(Arg0,Arg1,Arg2);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test2");
@@ -258,8 +232,6 @@ namespace PuertsStaticWrap
                 Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
             }
         }
-        
-        
         
         [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
         private static void M_Test3(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
@@ -284,9 +256,7 @@ namespace PuertsStaticWrap
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
                         
-                        
                     }
-                    
                 }
                 
                 
@@ -296,8 +266,6 @@ namespace PuertsStaticWrap
                 Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
             }
         }
-        
-        
         
         [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
         private static void M_Test4(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
@@ -328,10 +296,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test4(Arg0,Arg1,Arg2,Arg3);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 3)
@@ -353,10 +319,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test4(Arg0,Arg1,Arg2);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 if (paramLen == 2)
@@ -375,10 +339,8 @@ namespace PuertsStaticWrap
                         var result = obj.Test4(Arg0,Arg1);
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
                         return;
                     }
-                    
                 }
                 
                 Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test4");
@@ -389,8 +351,6 @@ namespace PuertsStaticWrap
             }
         }
         
-        
-        
         [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
         private static void M_Test5(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
         {
@@ -398,7 +358,7 @@ namespace PuertsStaticWrap
             {
                 var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
                 
-                if (paramLen >= 2)
+                
                 {
                     
                     var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
@@ -406,9 +366,7 @@ namespace PuertsStaticWrap
                     var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
                     
                     
-                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
-                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
-                        && argHelper2.IsMatchParams(Puerts.JsValueType.Boolean, null, 2, paramLen))
+                    
                     {
                         
                         var Arg0 = argHelper0.GetString(false);
@@ -418,34 +376,10 @@ namespace PuertsStaticWrap
                         
                         Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
                         
-                        return;
                     }
-                    
                 }
                 
-                if (paramLen == 2)
-                {
-                    
-                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
-                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
-                    
-                    
-                    if (argHelper0.IsMatch(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, false, false)
-                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false))
-                    {
-                        
-                        var Arg0 = argHelper0.GetString(false);
-                        var Arg1 = argHelper1.GetInt32(false);
-                        var result = obj.Test5(Arg0,Arg1);
-                        
-                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
-                        return;
-                    }
-                    
-                }
                 
-                Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test5");
             }
             catch (Exception e)
             {
@@ -453,95 +387,6 @@ namespace PuertsStaticWrap
             }
         }
         
-        
-        
-        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
-        private static void M_Test6(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
-        {
-            try
-            {
-                var obj = Puerts.Utils.GetSelf((int)data, self) as Puerts.UnitTest.OptionalParametersClass;
-                
-                if (paramLen >= 2)
-                {
-                    
-                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
-                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
-                    var argHelper2 = new Puerts.ArgumentHelper((int)data, isolate, info, 2);
-                    
-                    
-                    if (argHelper0.IsMatch(Puerts.JsValueType.Number, null, false, false)
-                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false)
-                        && argHelper2.IsMatchParams(Puerts.JsValueType.NullOrUndefined | Puerts.JsValueType.String, null, 2, paramLen))
-                    {
-                        
-                        var Arg0 = argHelper0.GetInt32(false);
-                        var Arg1 = argHelper1.GetInt32(false);
-                        var Arg2 = argHelper2.GetParams<string>(info, 2, paramLen);
-                        var result = obj.Test6(Arg0,Arg1,Arg2);
-                        
-                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
-                        return;
-                    }
-                    
-                }
-                
-                if (paramLen == 2)
-                {
-                    
-                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
-                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
-                    
-                    
-                    if (argHelper0.IsMatch(Puerts.JsValueType.Number, null, false, false)
-                        && argHelper1.IsMatch(Puerts.JsValueType.Number, null, false, false))
-                    {
-                        
-                        var Arg0 = argHelper0.GetInt32(false);
-                        var Arg1 = argHelper1.GetInt32(false);
-                        var result = obj.Test6(Arg0,Arg1);
-                        
-                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
-                        return;
-                    }
-                    
-                }
-                
-                if (paramLen == 1)
-                {
-                    
-                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
-                    
-                    
-                    if (argHelper0.IsMatch(Puerts.JsValueType.Number, null, false, false))
-                    {
-                        
-                        var Arg0 = argHelper0.GetInt32(false);
-                        var result = obj.Test6(Arg0);
-                        
-                        Puerts.PuertsDLL.ReturnNumber(isolate, info, result);
-                        
-                        return;
-                    }
-                    
-                }
-                
-                Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to Test6");
-            }
-            catch (Exception e)
-            {
-                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
-            }
-        }
-        
-        
-        
-        
-
-        
-
         
         
         
@@ -554,39 +399,14 @@ namespace PuertsStaticWrap
                 Constructor = Constructor,
                 Methods = new System.Collections.Generic.Dictionary<Puerts.MethodKey, Puerts.V8FunctionCallback>()
                 {
-                    
-                    { new Puerts.MethodKey {Name = "Test", IsStatic = false},  null},
-                    
-                    { new Puerts.MethodKey {Name = "Test2", IsStatic = false},  null},
-                    
-                    { new Puerts.MethodKey {Name = "Test3", IsStatic = false},  null},
-                    
-                    { new Puerts.MethodKey {Name = "Test4", IsStatic = false},  null},
-                    
-                    { new Puerts.MethodKey {Name = "Test5", IsStatic = false},  null},
-                    
-                    { new Puerts.MethodKey {Name = "Test6", IsStatic = false},  null},
-                    
-                    
-                    
-                    
+                    { new Puerts.MethodKey {Name = "Test", IsStatic = false},  M_Test },
+                    { new Puerts.MethodKey {Name = "Test2", IsStatic = false},  M_Test2 },
+                    { new Puerts.MethodKey {Name = "Test3", IsStatic = false},  M_Test3 },
+                    { new Puerts.MethodKey {Name = "Test4", IsStatic = false},  M_Test4 },
+                    { new Puerts.MethodKey {Name = "Test5", IsStatic = false},  M_Test5 },
                     
                 },
                 Properties = new System.Collections.Generic.Dictionary<string, Puerts.PropertyRegisterInfo>()
-                {
-                    
-                },
-                LazyMethods = new System.Collections.Generic.Dictionary<Puerts.MethodKey, Puerts.V8FunctionCallback>()
-                {
-                    
-                    { new Puerts.MethodKey {Name = "TestFilter", IsStatic = false},  null},
-                    
-                    
-                    
-                    
-                    
-                },
-                LazyProperties = new System.Collections.Generic.Dictionary<string, Puerts.PropertyRegisterInfo>()
                 {
                     
                 }
