0d036bc344757c89aa6a78c3b8c651885ab85d0f
zombieyang <348063288@qq.com>
2021-11-25 12:21:57 +0800

    .

--- quickjs-build/make_osx.sh
+++ quickjs-build/make_osx.sh
@@ -1,6 +1,6 @@
 mkdir -p build && cd build
 cmake -GXcode ../
 cd ..
-cmake --build build --config Debug
+cmake --build build --config Release
 mkdir -p ~/qjs/quickjs/Lib/macOS/
 cp build/Release/libquickjs.a ~/qjs/quickjs/Lib/macOS/
