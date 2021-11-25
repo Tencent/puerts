4ab2ce3a20acb75d7e3c21e85e16e1368b88cf7d
zombieyang <348063288@qq.com>
2021-11-25 16:37:09 +0800

    Release

--- quickjs-build/win/make_win64.bat
+++ quickjs-build/win/make_win64.bat
@@ -1,5 +1,5 @@
 mkdir build & pushd build
 cmake -S ..\CMakeLists.win.txt -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -G "Visual Studio 16 2019" -A x64 ..
 popd
-cmake --build build --config Debug
+cmake --build build --config Release
 pause
