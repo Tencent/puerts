6f679f4ec5a7f59596e696aae3d1e7db63fab72f
zombieyang <348063288@qq.com>
2021-11-25 18:18:51 +0800

    ut??

--- unity/Assets/Puerts/Src/Resources/puerts/log.mjs
+++ unity/Assets/Puerts/Src/Resources/puerts/log.mjs
@@ -10,40 +10,41 @@ var global = global || globalThis;
 
 let UnityEngine_Debug = puerts.loadType('UnityEngine.Debug');
 
-if (!UnityEngine_Debug) return;
+if (UnityEngine_Debug) {
+    
+    const console_org = global.console;
+    var console = {}
 
-const console_org = global.console;
-var console = {}
+    function toString(args) {
+        return Array.prototype.map.call(args, x => {
+            try {
+                return x+'';
+            } catch (err) {
+                return err;
+            }
+        }).join(',');
+    }
 
-function toString(args) {
-    return Array.prototype.map.call(args, x => {
-        try {
-            return x+'';
-        } catch (err) {
-            return err;
-        }
-    }).join(',');
+    console.log = function() {
+        if (console_org) console_org.log.apply(null, Array.prototype.slice.call(arguments));
+        UnityEngine_Debug.Log(toString(arguments));
+    }
+
+    console.info = function() {
+        if (console_org) console_org.info.apply(null, Array.prototype.slice.call(arguments));
+        UnityEngine_Debug.Log(toString(arguments));
+    }
+
+    console.warn = function() {
+        if (console_org) console_org.warn.apply(null, Array.prototype.slice.call(arguments));
+        UnityEngine_Debug.LogWarning(toString(arguments));
+    }
+
+    console.error = function() {
+        if (console_org) console_org.error.apply(null, Array.prototype.slice.call(arguments));
+        UnityEngine_Debug.LogError(toString(arguments));
+    }
+
+    global.console = console;
+    puerts.console = console;   
 }
-
-console.log = function() {
-    if (console_org) console_org.log.apply(null, Array.prototype.slice.call(arguments));
-    UnityEngine_Debug.Log(toString(arguments));
-}
-
-console.info = function() {
-    if (console_org) console_org.info.apply(null, Array.prototype.slice.call(arguments));
-    UnityEngine_Debug.Log(toString(arguments));
-}
-
-console.warn = function() {
-    if (console_org) console_org.warn.apply(null, Array.prototype.slice.call(arguments));
-    UnityEngine_Debug.LogWarning(toString(arguments));
-}
-
-console.error = function() {
-    if (console_org) console_org.error.apply(null, Array.prototype.slice.call(arguments));
-    UnityEngine_Debug.LogError(toString(arguments));
-}
-
-global.console = console;
-puerts.console = console;
