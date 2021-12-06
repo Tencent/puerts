496b555fca6eb3dd0615c47b75d85b08fdd347d8
zombieyang <348063288@qq.com>
2021-12-06 20:49:04 +0800

    [unity]refactor CMake

--- unity/native_src/make_android.sh
+++ unity/native_src/make_android.sh
@@ -1,3 +1,9 @@
+ENGINE=$1
+if [ "$1" == "" ]
+then
+    ENGINE="v8"
+fi
+
 if [ -n "$ANDROID_NDK" ]; then
     export NDK=${ANDROID_NDK}
 elif [ -n "$ANDROID_NDK_HOME" ]; then
@@ -15,8 +21,8 @@ function build() {
     API=$1
     ABI=$2
     TOOLCHAIN_ANME=$3
-    BUILD_PATH=build.Android.${ABI}
-    cmake -DANDROID_ABI=${ABI} -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -H. -B${BUILD_PATH} -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME}
+    BUILD_PATH=build.$ENGINE.Android.${ABI}
+    cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DJS_ENGINE=$ENGINE -DANDROID_ABI=${ABI} -H. -B${BUILD_PATH} -DCMAKE_TOOLCHAIN_FILE=${NDK}/build/cmake/android.toolchain.cmake -DANDROID_NATIVE_API_LEVEL=${API} -DANDROID_TOOLCHAIN=clang -DANDROID_TOOLCHAIN_NAME=${TOOLCHAIN_ANME}
     cmake --build ${BUILD_PATH} --config Release
     mkdir -p ../Assets/Plugins/Android/libs/${ABI}/
     cp ${BUILD_PATH}/libpuerts.so ../Assets/Plugins/Android/libs/${ABI}/libpuerts.so
--- unity/native_src/make_win32.bat
+++ unity/native_src/make_win32.bat
@@ -1,6 +1,12 @@
-mkdir build32 & pushd build32
+set ENGINE=%1
+if "%ENGINE%"=="" (
+  set ENGINE=v8
+)
+
+mkdir build32_%ENGINE% & pushd build32_%ENGINE%
 cmake -G "Visual Studio 16 2019" -A Win32 ..
 popd
-cmake --build build32 --config Release
+cmake --build build32_%ENGINE% --config Release
 md ..\Assets\Plugins\x86
-copy build32\Release\puerts.dll ..\Assets\Plugins\x86
+copy build32_%ENGINE%\Release\puerts.dll ..\Assets\Plugins\x86
+copy %ENGINE%\Lib\Win32\*.dll ..\Assets\Plugins\x86
