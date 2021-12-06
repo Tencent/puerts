a79fa8e6784d7f3a2ded91bcf488c2ca232f8ed8
僵尸浩 <348063288@qq.com>
2021-12-06 21:38:02 +0800

    [unity]重构CMake构建 (#567)

--- unity/native_src/make_linux64.sh
+++ unity/native_src/make_linux64.sh
@@ -1,6 +1,12 @@
-mkdir -p build_linux64 && cd build_linux64
-cmake ../
+ENGINE=$1
+if [ "$1" == "" ]
+then
+    ENGINE="v8"
+fi
+
+mkdir -p build_linux64_$ENGINE && cd build_linux64_$ENGINE
+cmake -DJS_ENGINE=$ENGINE ../
 cd ..
-cmake --build build_linux64 --config Release
+cmake --build build_linux64_$ENGINE --config Release
 mkdir -p ../Assets/Plugins/x86_64/
-cp build_linux64/libpuerts.so ../Assets/Plugins/x86_64/libpuerts.so
+cp build_linux64_$ENGINE/libpuerts.so ../Assets/Plugins/x86_64/libpuerts.so
