cb3fc6b7d707bfe6dd89b3014d092daa4dfe941a
bluedoom <bluedoom@qq.com>
2021-12-31 23:10:18 +0800

    支持 in 关键字 (#577)

---
+++ unity/general/Src/UnitTest/warp/PuertsTest_RefInTest_Wrap.cs
@@ -0,0 +1,105 @@
+﻿
+using System;
+namespace PuertsStaticWrap
+{
+    public static class PuertsTest_RefInTest_Wrap
+    {
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8ConstructorCallback))]
+        private static IntPtr Constructor(IntPtr isolate, IntPtr info, int paramLen, long data)
+        {
+            try
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "invalid arguments to PuertsTest.RefInTest constructor");
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+            return IntPtr.Zero;
+        }
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void F_Divide(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                {
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    {
+                        var Arg0 = argHelper0.Get<Puerts.UnitTest.RefInClass.Vector3>(false);
+                        var Arg1 = argHelper1.Get<Puerts.UnitTest.RefInClass.Vector3>(false);
+                        var result = Puerts.UnitTest.RefInClass.Divide(Arg0,Arg1);
+                        Puerts.ResultHelper.Set((int)data, isolate, info, result);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void F_Multiply(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                {
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    var argHelper1 = new Puerts.ArgumentHelper((int)data, isolate, info, 1);
+                    {
+                        var Arg0 = argHelper0.Get<Puerts.UnitTest.RefInClass.Vector3>(false);
+                        var Arg1 = argHelper1.Get<Puerts.UnitTest.RefInClass.Vector3>(false);
+                        var result = Puerts.UnitTest.RefInClass.Multiply(Arg0,Arg1);
+                        Puerts.ResultHelper.Set((int)data, isolate, info, result);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        [Puerts.MonoPInvokeCallback(typeof(Puerts.V8FunctionCallback))]
+        private static void F_Normalize(IntPtr isolate, IntPtr info, IntPtr self, int paramLen, long data)
+        {
+            try
+            {
+                {
+                    var argHelper0 = new Puerts.ArgumentHelper((int)data, isolate, info, 0);
+                    {
+                        var Arg0 = argHelper0.Get<Puerts.UnitTest.RefInClass.Vector3>(false);
+                        var result = Puerts.UnitTest.RefInClass.Normalize(Arg0);
+                        Puerts.ResultHelper.Set((int)data, isolate, info, result);
+                    }
+                }
+            }
+            catch (Exception e)
+            {
+                Puerts.PuertsDLL.ThrowException(isolate, "c# exception:" + e.Message + ",stack:" + e.StackTrace);
+            }
+        }
+        public static Puerts.TypeRegisterInfo GetRegisterInfo()
+        {
+            return new Puerts.TypeRegisterInfo()
+            {
+                BlittableCopy = false,
+                Constructor = Constructor,
+                Methods = new System.Collections.Generic.Dictionary<Puerts.MethodKey, Puerts.V8FunctionCallback>()
+                {
+                    { new Puerts.MethodKey {Name = "Divide", IsStatic = true}, F_Divide },
+                    { new Puerts.MethodKey {Name = "Multiply", IsStatic = true}, F_Multiply },
+                    { new Puerts.MethodKey {Name = "Normalize", IsStatic = true}, F_Normalize },
+                },
+                Properties = new System.Collections.Generic.Dictionary<string, Puerts.PropertyRegisterInfo>()
+                {
+                },
+                LazyMethods = new System.Collections.Generic.Dictionary<Puerts.MethodKey, Puerts.V8FunctionCallback>()
+                {
+                },
+                LazyProperties = new System.Collections.Generic.Dictionary<string, Puerts.PropertyRegisterInfo>()
+                {
+                }
+            };
+        }
+    }
+}
