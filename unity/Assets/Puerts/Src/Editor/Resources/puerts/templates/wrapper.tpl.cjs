c026b37c8ee1d51d6d8cd028fbef4f7983b4f237
zombieyang <348063288@qq.com>
2022-01-12 14:37:46 +0800

    generate bug fix

--- unity/Assets/Puerts/Src/Editor/Resources/puerts/templates/wrapper.tpl.cjs
+++ unity/Assets/Puerts/Src/Editor/Resources/puerts/templates/wrapper.tpl.cjs
@@ -285,10 +285,12 @@ namespace PuertsStaticWrap
                 tt`
                         ${overload.IsVoid ? "" : "var result = "}${method.IsStatic ? data.Name : refSelf()}.${UnK(method.Name)}(${toJsArray(overload.ParameterInfos).map(paramListLambda).join(', ')});
                 `
-                toJsArray(overload.ParameterInfos).filter(p => p.IsByRef).forEach((paramInfo, idx) => {
-                    tt`
-                        argHelper${idx}.SetByRefValue(Arg${idx});
-                    `
+                toJsArray(overload.ParameterInfos).forEach((paramInfo, idx) => {
+                    if (paramInfo.isByRef) {
+                        tt`
+                            argHelper${idx}.SetByRefValue(Arg${idx});
+                        `
+                    }
                 })
                 tt`
                         ${!overload.IsVoid ? setReturn(overload) + ';' : ''}
