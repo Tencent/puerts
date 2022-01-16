5b3674c98e90004c810833db01a1d1df8ae0168d
zombieyang <zombieyang@tencent.com>
2022-01-16 18:47:37 +0800

    push to wrong remote

--- nodejs-build/linux.sh
+++ nodejs-build/linux.sh
@@ -3,7 +3,7 @@
 VERSION=$1
 
 cd ~
-git clone -b v16.x https://github.com/nodejs/node.git
+git clone --single-branch -b v14.x https://github.com/nodejs/node.git
 
 cd node
 git fetch origin v$VERSION
