6d65e33f2778614170cc1450f6842a27d80d0a8e
僵尸浩 <zombieyang@tencent.com>
2022-05-20 12:01:03 +0800

    Revert "导出index.d.ts时重名类报错的问题 (#823) fix#657" (#824)
    This reverts commit 53839d93b1bc9eb570db0d2396234a4c97f50349.

--- unity/Assets/Puerts/Editor/Src/Generator/DTS.cs
+++ unity/Assets/Puerts/Editor/Src/Generator/DTS.cs
@@ -10,7 +10,6 @@ using System.Collections.Generic;
 using System.Reflection;
 using System.Linq;
 using System.Runtime.CompilerServices;
-using UnityEngine;
 
 namespace Puerts.Editor
 {
@@ -466,14 +465,7 @@ namespace Puerts.Editor
                     foreach (var t in refTypes.Distinct())
                     {
                         var info = TsTypeGenInfo.FromType(t, genTypeSet);
-                        try 
-                        {
-                            tsTypeGenInfos.Add(info.FullName, info);                     
-                        }
-                        catch (Exception e)
-                        {
-                            Debug.LogError($"Add {info.FullName} failed!\n{e.ToString()}");
-                        }
+                        tsTypeGenInfos.Add(info.FullName, info);
                     }
                     foreach (var info in tsTypeGenInfos)
                     {
