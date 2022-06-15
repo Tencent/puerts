df138b17d54a7f477fea5da4d5673ec84779e2cf
zombieyang(杨浩) <zombieyang@tencent.com>
2022-06-15 16:41:15 +0800

    [unity] add unittest

--- unity/general/Src/UnitTest/WrapperGenTest.cs
+++ unity/general/Src/UnitTest/WrapperGenTest.cs
@@ -1,4 +1,6 @@
 using NUnit.Framework;
+using System;
+using System.Collections.Generic;
 
 namespace Puerts.UnitTest 
 {
@@ -110,6 +112,34 @@ namespace Puerts.UnitTest
     [TestFixture]
     public class WrapperGenUnitTest
     {
+        [Test]
+        public void GenericWrapper()
+        {
+
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() { typeof(Dictionary<int, JsEnv>) };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<int, JsEnv>), genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True(wrapperContent.Contains("<T>"));
+        }
+
+        [Test]
+        public void GenericWrapper_2()
+        {
+
+            var jsEnv = new JsEnv(new TxtLoader());
+            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+            var genList = new List<Type>() {
+                typeof(Dictionary<Type, JsEnv>)             
+            };
+            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<Type, JsEnv>), genList);
+
+            string wrapperContent = wrapRender(wrapperInfo);
+            Assert.True(wrapperContent.Contains("<T,S>"));
+        }
+
         [Test]
         public void PropertyTest()
         {
