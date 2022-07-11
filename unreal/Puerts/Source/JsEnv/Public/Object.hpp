47ad93e349506859c7edcc95b60c2bc3c93c1339
johnche <johnche@tencent.com>
2022-07-11 19:48:13 +0800

    [unreal]pesapi兼容

--- unreal/Puerts/Source/JsEnv/Public/Object.hpp
+++ unreal/Puerts/Source/JsEnv/Public/Object.hpp
@@ -27,18 +27,18 @@ namespace converter
 template <typename R, typename... Args>
 struct Converter<std::function<R(Args...)>>
 {
-    static v8::Local<v8::Value> toScript(v8::Local<v8::Context> context, std::function<R(Args...)> value)
+    static ValueType toScript(ContextType context, std::function<R(Args...)> value)
     {
         return GetUndefined(context);
     }
 
-    static std::function<R(Args...)> toCpp(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+    static std::function<R(Args...)> toCpp(ContextType context, const ValueType value)
     {
         Function PF(context, value.As<v8::Object>());
         return [=](Args... cppArgs) -> R { return PF.Func<R>(cppArgs...); };
     }
 
-    static bool accept(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+    static bool accept(ContextType context, const ValueType value)
     {
         return Converter<Function>::accept(context, value);
     }
@@ -47,18 +47,18 @@ struct Converter<std::function<R(Args...)>>
 template <typename... Args>
 struct Converter<std::function<void(Args...)>>
 {
-    static v8::Local<v8::Value> toScript(v8::Local<v8::Context> context, std::function<void(Args...)> value)
+    static ValueType toScript(ContextType context, std::function<void(Args...)> value)
     {
         return GetUndefined(context);
     }
 
-    static std::function<void(Args...)> toCpp(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+    static std::function<void(Args...)> toCpp(ContextType context, const ValueType value)
     {
         Function PF(context, value.As<v8::Object>());
         return [=](Args... cppArgs) -> void { PF.Action(cppArgs...); };
     }
 
-    static bool accept(v8::Local<v8::Context> context, const v8::Local<v8::Value>& value)
+    static bool accept(ContextType context, const ValueType value)
     {
         return Converter<Function>::accept(context, value);
     }
