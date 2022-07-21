0c76ab158bea9c9c37364e657ce3e38ecb29dfc0
johnche <johnche@tencent.com>
2022-07-21 11:52:54 +0800

    [unreal]纯c++实现改回使用std::map

--- unreal/Puerts/Source/JsEnv/Private/CppObjectMapper.h
+++ unreal/Puerts/Source/JsEnv/Private/CppObjectMapper.h
@@ -12,7 +12,7 @@
 #include "v8.h"
 #pragma warning(pop)
 
-#include <unordered_map>
+#include <map>
 #include "JSClassRegister.h"
 #include "ObjectCacheNode.h"
 #include "ObjectMapper.h"
@@ -41,13 +41,13 @@ public:
     v8::Local<v8::FunctionTemplate> GetTemplateOfClass(v8::Isolate* Isolate, const JSClassDefinition* ClassDefinition);
 
 private:
-    std::unordered_map<void*, FObjectCacheNode> CDataCache;
+    std::map<void*, FObjectCacheNode> CDataCache;
 
-    std::unordered_map<const void*, v8::UniquePersistent<v8::FunctionTemplate>> CDataNameToTemplateMap;
+    std::map<const void*, v8::UniquePersistent<v8::FunctionTemplate>> CDataNameToTemplateMap;
 
     v8::UniquePersistent<v8::Function> PointerConstructor;
 
-    std::unordered_map<void*, FinalizeFunc> CDataFinalizeMap;
+    std::map<void*, FinalizeFunc> CDataFinalizeMap;
 };
 
 }    // namespace puerts
