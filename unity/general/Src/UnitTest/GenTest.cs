8b568c73bc03ee4cb600b4a9ac178025e6046a6b
zombieyang(杨浩) <zombieyang@tencent.com>
2022-07-28 20:39:46 +0800

    [unity] disable ut also

--- unity/general/Src/UnitTest/GenTest.cs
+++ unity/general/Src/UnitTest/GenTest.cs
@@ -11,62 +11,62 @@ namespace Puerts.UnitTest
     [TestFixture]
     public class GenUnitTest
     {
-        [Test]
-        public void GenericWrapper()
-        {
+        // [Test]
+        // public void GenericWrapper()
+        // {
 
-            var jsEnv = new JsEnv(new TxtLoader());
-            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
-            var genList = new List<Type>() { typeof(Dictionary<int, JsEnv>) };
-            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<int, JsEnv>), genList);
+        //     var jsEnv = new JsEnv(new TxtLoader());
+        //     var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+        //     var genList = new List<Type>() { typeof(Dictionary<int, JsEnv>) };
+        //     Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<int, JsEnv>), genList);
 
-            string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T>"));
-        }
+        //     string wrapperContent = wrapRender(wrapperInfo);
+        //     Assert.True(wrapperContent.Contains("<T>"));
+        // }
 
-        [Test]
-        public void GenericWrapper_2()
-        {
+        // [Test]
+        // public void GenericWrapper_2()
+        // {
 
-            var jsEnv = new JsEnv(new TxtLoader());
-            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
-            var genList = new List<Type>() {
-                typeof(Dictionary<Type, JsEnv>)             
-            };
-            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<Type, JsEnv>), genList);
+        //     var jsEnv = new JsEnv(new TxtLoader());
+        //     var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+        //     var genList = new List<Type>() {
+        //         typeof(Dictionary<Type, JsEnv>)             
+        //     };
+        //     Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Dictionary<Type, JsEnv>), genList);
 
-            string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T,S>"));
-        }
+        //     string wrapperContent = wrapRender(wrapperInfo);
+        //     Assert.True(wrapperContent.Contains("<T,S>"));
+        // }
 
-        [Test]
-        public void GenericWrapper_3()
-        {
-            var jsEnv = new JsEnv(new TxtLoader());
-            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
-            var genList = new List<Type>() {
-                typeof(Singleton1<Zombie>),            
-                typeof(Zombie)             
-            };
-            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton1<Zombie>), genList);
+        // [Test]
+        // public void GenericWrapper_3()
+        // {
+        //     var jsEnv = new JsEnv(new TxtLoader());
+        //     var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+        //     var genList = new List<Type>() {
+        //         typeof(Singleton1<Zombie>),            
+        //         typeof(Zombie)             
+        //     };
+        //     Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton1<Zombie>), genList);
 
-            string wrapperContent = wrapRender(wrapperInfo);
-            Assert.False(wrapperContent.Contains("<T>"));
-        }
+        //     string wrapperContent = wrapRender(wrapperInfo);
+        //     Assert.False(wrapperContent.Contains("<T>"));
+        // }
 
-        [Test]
-        public void GenericWrapper_4()
-        {
-            var jsEnv = new JsEnv(new TxtLoader());
-            var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
-            var genList = new List<Type>() {
-                typeof(Singleton2<Zombie>),            
-                typeof(Zombie)             
-            };
-            Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton2<Zombie>), genList);
+        // [Test]
+        // public void GenericWrapper_4()
+        // {
+        //     var jsEnv = new JsEnv(new TxtLoader());
+        //     var wrapRender = jsEnv.Eval<Func<Editor.Generator.Wrapper.StaticWrapperInfo, string>>("require('puerts/templates/wrapper.tpl.cjs')");
+        //     var genList = new List<Type>() {
+        //         typeof(Singleton2<Zombie>),            
+        //         typeof(Zombie)             
+        //     };
+        //     Editor.Generator.Wrapper.StaticWrapperInfo wrapperInfo = Editor.Generator.Wrapper.StaticWrapperInfo.FromType(typeof(Singleton2<Zombie>), genList);
 
-            string wrapperContent = wrapRender(wrapperInfo);
-            Assert.True(wrapperContent.Contains("<T>"));
-        }
+        //     string wrapperContent = wrapRender(wrapperInfo);
+        //     Assert.True(wrapperContent.Contains("<T>"));
+        // }
     }
 }
