2eb3d9b70739ea993fc3b5566aa40ab515e34d80
僵尸浩 <348063288@qq.com>
2022-09-22 20:27:28 +0800

    Revert "[unity] jsdoc gen fix 2"
    This reverts commit 9fef6fca3968d9dc44ec8c6ad9a0a69f535e3ded.

--- unity/Assets/Puerts/Editor/Src/DocResolver.cs
+++ unity/Assets/Puerts/Editor/Src/DocResolver.cs
@@ -118,10 +118,10 @@ namespace Puerts
             {
                 var ext = Path.GetExtension(location);
                 var xmlFilePath = location.Substring(0, location.Length - ext.Length) + ".xml";
-                //增加文档查找路径 
+                //LHC 增加文档查找路径 
                 if (!File.Exists(xmlFilePath))
                 {
-                    xmlFilePath = xmlFilePath.Replace("Library\\ScriptAssemblies\\", "Library\\CommonetDocs\\");
+                    xmlFilePath = xmlFilePath.Replace("Library\\ScriptAssemblies\\", "Temp\\bin\\Debug\\");
                 }
                 return resolver.ParseXml(xmlFilePath);
             }
@@ -293,10 +293,11 @@ namespace Puerts
                     var pname = reader.GetAttribute("name");
                     var ptext = ReadSingleTextBlock(reader, body, "param");
 
-                    if (!string.IsNullOrEmpty(ptext))
-                    {
-                        body.parameters[pname] = ptext;
-                    }
+                    // if (!string.IsNullOrEmpty(ptext))
+                    // {
+                    //     body.parameters[pname] = ptext;
+                    // }
+                    body.parameters[pname] = ptext;
                 }
 
                 if (type == XmlNodeType.Element && reader.Name == "returns")
