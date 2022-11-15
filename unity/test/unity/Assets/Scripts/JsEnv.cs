3a76363117e4f8ce248b9f76205fbfdb239f5803
zombieyang <zombieyang@tencent.com>
2022-11-15 17:12:27 +0800

    move dotnet-test to test/dotnet, add test/unity

---
+++ unity/test/unity/Assets/Scripts/JsEnv.cs
@@ -0,0 +1,114 @@
+/*
+* Tencent is pleased to support the open source community by making Puerts available.
+* Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+* Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
+* This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
+*/
+
+using System;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace Puerts
+{
+    public class JsEnv : IDisposable
+    {
+        IntPtr nativeJsEnv;
+        IntPtr nativePesapiEnv;
+
+        Type persistentObjectInfoType;
+        MethodInfo objectPoolAddMethodInfo;
+        MethodInfo objectPoolRemoveMethodInfo;
+        MethodInfo tryLoadTypeMethodInfo;
+
+        PuertsIl2cpp.ObjectPool objectPool = new PuertsIl2cpp.ObjectPool();
+
+        public JsEnv()
+        {
+            //only once is enough
+            PuertsIl2cpp.NativeAPI.SetLogCallback(PuertsIl2cpp.NativeAPI.Log);
+            PuertsIl2cpp.NativeAPI.InitialPuerts(PuertsIl2cpp.NativeAPI.GetPesapiImpl());
+            PuertsIl2cpp.NativeAPI.ExchangeAPI(PuertsIl2cpp.NativeAPI.GetUnityExports());
+            tryLoadTypeMethodInfo = typeof(PuertsIl2cpp.NativeAPI).GetMethod("RegisterNoThrow");
+            PuertsIl2cpp.NativeAPI.SetTryLoadCallback(PuertsIl2cpp.NativeAPI.GetMethodInfoPointer(tryLoadTypeMethodInfo), PuertsIl2cpp.NativeAPI.GetMethodPointer(tryLoadTypeMethodInfo));
+
+            persistentObjectInfoType = typeof(PuertsIl2cpp.PersistentObjectInfo);
+            PuertsIl2cpp.NativeAPI.SetPersistentObjectInfoType(persistentObjectInfoType);
+
+            nativeJsEnv = PuertsIl2cpp.NativeAPI.CreateNativeJSEnv();
+            nativePesapiEnv = PuertsIl2cpp.NativeAPI.GetPesapiEnvHolder(nativeJsEnv);
+
+            //PuertsIl2cpp.NativeAPI.SetObjectPool(objectPool, typeof(PuertsIl2cpp.ObjectPool).GetMethod("Add")); //TODO: remove....
+            objectPoolAddMethodInfo = typeof(PuertsIl2cpp.ObjectPool).GetMethod("Add");
+            objectPoolRemoveMethodInfo = typeof(PuertsIl2cpp.ObjectPool).GetMethod("Remove");
+            PuertsIl2cpp.NativeAPI.SetObjectPool(nativeJsEnv, PuertsIl2cpp.NativeAPI.GetMethodInfoPointer(objectPoolAddMethodInfo), PuertsIl2cpp.NativeAPI.GetMethodPointer(objectPoolAddMethodInfo),
+                PuertsIl2cpp.NativeAPI.GetMethodInfoPointer(objectPoolRemoveMethodInfo), PuertsIl2cpp.NativeAPI.GetMethodPointer(objectPoolRemoveMethodInfo),
+                PuertsIl2cpp.NativeAPI.GetObjectPointer(objectPool));
+
+            PuertsIl2cpp.NativeAPI.SetObjectToGlobal(nativeJsEnv, "jsEnv", PuertsIl2cpp.NativeAPI.GetObjectPointer(this));
+        }
+
+        public Type GetTypeByString(string className)
+        {
+            return TypeUtils.GetType(className);
+        }
+
+        public void Eval(string chunk, string chunkName = "chunk")
+        {
+#if THREAD_SAFE
+            lock(this) {
+#endif
+            PuertsIl2cpp.NativeAPI.EvalInternal(nativePesapiEnv, System.Text.Encoding.UTF8.GetBytes(chunk), chunkName, null);
+#if THREAD_SAFE
+            }
+#endif
+        }
+
+        public T Eval<T>(string chunk, string chunkName = "chunk")
+        {
+#if THREAD_SAFE
+            lock(this) {
+#endif
+            return (T)PuertsIl2cpp.NativeAPI.EvalInternal(nativePesapiEnv, System.Text.Encoding.UTF8.GetBytes(chunk), chunkName, typeof(T));
+#if THREAD_SAFE
+            }
+#endif
+        }
+
+        ~JsEnv()
+        {
+#if THREAD_SAFE
+            lock(this) {
+#endif
+            Dispose(true);
+#if THREAD_SAFE
+            }
+#endif
+        }
+
+        public void Dispose()
+        {
+#if THREAD_SAFE
+            lock(this) {
+#endif
+            Dispose(true);
+#if THREAD_SAFE
+            }
+#endif
+        }
+
+        private bool disposed = false;
+
+        protected virtual void Dispose(bool dispose)
+        {
+            lock (this)
+            {
+                if (disposed) return;
+                // TODO: nativePesapiEnv release
+                PuertsIl2cpp.NativeAPI.DestroyNativeJSEnv(nativeJsEnv);
+                disposed = true;
+            }
+        }
+    }
+}
---
+++ unity/test/unity/Assets/Scripts/TypeUtils.cs
@@ -0,0 +1,56 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using System.Reflection;
+
+namespace Puerts
+{
+    public static class TypeUtils
+    {
+        private static Type GetType(string className, bool isQualifiedName)
+        {
+            Type type = Type.GetType(className, false);
+            if (type != null)
+            {
+                return type;
+            }
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                type = assembly.GetType(className);
+
+                if (type != null)
+                {
+                    return type;
+                }
+            }
+            int p1 = className.IndexOf('[');
+            if (p1 > 0 && !isQualifiedName)
+            {
+                string qualified_name = className.Substring(0, p1 + 1);
+                string[] generic_params = className.Substring(p1 + 1, className.Length - qualified_name.Length - 1).Split(',');
+                for (int i = 0; i < generic_params.Length; i++)
+                {
+                    Type generic_param = GetType(generic_params[i].Trim(), false);
+                    if (generic_param == null)
+                    {
+                        return null;
+                    }
+                    if (i != 0)
+                    {
+                        qualified_name += ", ";
+                    }
+                    qualified_name = qualified_name + "[" + generic_param.AssemblyQualifiedName + "]";
+                }
+                qualified_name += "]";
+                return GetType(qualified_name, true);
+            }
+            return null;
+        }
+
+        public static Type GetType(string className)
+        {
+            return GetType(className, false);
+        }
+    }
+}
