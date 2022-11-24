12dc68b174fa5e49878e8b58a5ec796b426a40af
zombieyang <348063288@qq.com>
2022-11-24 15:59:20 +0800

    bump core to 1.4.0. add some comment

--- package/Editor/WebGLPostProcessor.cs
+++ package/Editor/WebGLPostProcessor.cs
@@ -6,6 +6,22 @@ using System;
 using System.IO;
 using System.Collections.Generic;
 
+/**
+ * ！！！！！！！！！！必读！！！！！！！
+ * 由于本架构里，JS文件是运行在浏览器宿主JS引擎上的，因此，JS应当脱离Unity的资源系统进行加载（尤其是微信小游戏里，必须这样做）
+ * 所以，JsEnv.ExecuteModule执行JS时，并不会使用Loader进行JS加载。
+ *
+ * 那么JS是怎么执行的呢？
+ * 答案在 Javascripts/PuertsDLLMock/index.ts的ExecuteModule函数中。
+ * 在浏览器里，Puer会找全局变量PUERTS_JS_RESOURCES，它是一个key-value结构，记载着ExecuteModule会使用到的所有JS的内容。
+ * 在微信小游戏里，则是直接将ExecuteModule接收到的JS名传递给微信的require，require会去找puerts_minigame_js_resources下的JS。
+ *
+ * 而浏览器上的PUERTS_JS_RESOURCES，以及微信小游戏的JS目录，就是靠本PostProcessor生成的
+ * 本文件的作用就是收集Puerts.DefaultLoader用到的所有JS，并将它们构建为PUERTS_JS_RESOURCES和微信小游戏 puerts_minigame_js_resources 目录。
+ * 
+ * 如果你的游戏里使用了自定义的Loader，那么我建议您自己重新实现一个PostProcessor，以和你的Loader配套。
+ */
+
 public class WebGLPuertsPostProcessor {
 
     public static List<string> fileGlobbers = new List<string> 
