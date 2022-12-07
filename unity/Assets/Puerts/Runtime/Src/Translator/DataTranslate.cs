c45d04513a8906df58cf3e6063561aa13abd11f1
zombieyang <348063288@qq.com>
2022-12-07 15:46:08 +0800

    Revert "紧急修复"
    This reverts commit c37c4654f0bf337e797e54bdb4a5d5a79a5e4924.

--- unity/Assets/Puerts/Runtime/Src/Translator/DataTranslate.cs
+++ unity/Assets/Puerts/Runtime/Src/Translator/DataTranslate.cs
@@ -191,7 +191,7 @@ namespace Puerts
 
                     return result;
                 case JsValueType.Number:
-                    return getValueApi.GetNumber(isolate, value, isByRef);
+                    return DoubleTranslator(jsEnvIdx, isolate, getValueApi, value, isByRef);
                 case JsValueType.String:
                     return StringTranslator(jsEnvIdx, isolate, getValueApi, value, isByRef);
                 default:
