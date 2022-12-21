b170c3f61f7f268900ad288084563aefb1843aec
ps5mh <ps5mhg@gmail.com>
2022-12-21 14:47:12 +0800

    [unity] 2.0.x csharp module for #1055 - method.Invoke ref/short support & test case

--- unity/test/Src/Resources/CSharpModuleTest/generic_method_test.mjs
+++ unity/test/Src/Resources/CSharpModuleTest/generic_method_test.mjs
@@ -1,25 +1,33 @@
 let Assert_AreEqual = CS.NUnit.Framework.Assert.AreEqual
 
+// public int Boo<A>(short x) {return 4;}
 // public int Foo<A>() {return 1;}
-// // public void Foo<A>(ref int x) { x = 2; } // ref not supported now
+// public void Foo<A>(ref int x) { x += 2; }
 // public int Foo<A, B>() {return 3;}
-// public void Foo<A, B>(int x) { x = 4; }
+// public void Foo<A, B>(int x) { }
 // public C Foo<A, B, C>(out int x) { x = 5; return default; }
 // x.Foo<int>();
+// x.Boo<int>(0);
+// int z = 1;
+// x.Foo<int>(ref z);
 // x.Foo<int, string>();
-// x.Foo<int, string>();
+// x.Foo<int, string>(1);
 // x.Foo<int, string, int>(out var y);
 let typeof_GenericMethodTest = puer.$typeof(CS.Puerts.UnitTest.GenericMethodTest)
 let Foo_Int32 = puer.getGenericMethod(typeof_GenericMethodTest, "Foo", CS.System.Int32)
+let Boo_Int32 = puer.getGenericMethod(typeof_GenericMethodTest, "Boo", CS.System.Int32)
 let Foo_Int32_String = puer.getGenericMethod(typeof_GenericMethodTest, "Foo", CS.System.Int32, CS.System.String)
 let Foo_Int32_String_Int32 = puer.getGenericMethod(typeof_GenericMethodTest, "Foo", CS.System.Int32, CS.System.String, CS.System.Int32)
 
 let a = puer.$ref(9)
 let t = new CS.Puerts.UnitTest.GenericMethodTest()
 Assert_AreEqual(Foo_Int32.call(t), 1)
+Foo_Int32.call(t, a) // ref test
+Assert_AreEqual(puer.$unref(a), 11)
+Assert_AreEqual(Boo_Int32.call(t, 1), 4) // convert arg type test
 Assert_AreEqual(Foo_Int32_String.call(t), 3)
-Foo_Int32_String.call(t, 0)
-Foo_Int32_String_Int32.call(t, a)
+Foo_Int32_String.call(t, 0) // override test
+Foo_Int32_String_Int32.call(t, a) // out test
 Assert_AreEqual(puer.$unref(a), 5)
 
 // performance test
