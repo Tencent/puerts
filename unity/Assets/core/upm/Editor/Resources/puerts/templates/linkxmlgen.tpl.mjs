314b9a4ff7653f040dd984687533e9448662efe5
zombieyang <zombieyang@tencent.com>
2023-04-18 19:25:23 +0800

    [unity] optimize generic link.xml generate fix issue4 in #1288

--- unity/Assets/core/upm/Editor/Resources/puerts/templates/linkxmlgen.tpl.mjs
+++ unity/Assets/core/upm/Editor/Resources/puerts/templates/linkxmlgen.tpl.mjs
@@ -1,12 +1,34 @@
 import { FOR } from './tte.mjs'
 
-export default function TypingTemplate(genTypes) {
+export function LinkXMLTemplate(genTypes) {
     return `
 <linker>${FOR(getAssemblyInfo(genTypes), assemblyInfo => `
     <assembly fullname="${assemblyInfo.name}">${FOR(assemblyInfo.types, type => `
         <type fullname="${type}" preserve="all"/>`)}
     </assembly>`)}
 </linker>`.trim();
+}
+
+export function GenericTypePreserverTemplate(genTypes) {
+    return `
+namespace Puerts
+{
+    #if ENABLE_IL2CPP
+        [UnityEngine.Scripting.Preserve]
+    #endif
+    public static class GenericTypePreserver_Gen
+    {
+        #if ENABLE_IL2CPP
+            [UnityEngine.Scripting.Preserve]
+        #endif
+        public static void Preserver()
+        {
+        ${FOR(listToJsArray(genTypes).filter(item=> item.IsGenericType), item => `
+            UnityEngine.Debug.Log(typeof(${item.GetFriendlyName()}));
+        `)}
+        }
+    }
+}`.trim();
 }
 
 function getAssemblyInfo(genTypes) {
@@ -17,7 +39,19 @@ function getAssemblyInfo(genTypes) {
         let assemblyName = type.Assembly.GetName(false).Name;
         !assemblyInfo.has(assemblyName) && assemblyInfo.set(assemblyName, [])
         let types = assemblyInfo.get(assemblyName)
-        types.push(type.FullName)
+        if (type.IsGenericType) {
+            types.push(type.FullName.split('[')[0])
+        } else {
+            types.push(type.FullName)
+        }
     }
     return Array.from(assemblyInfo).map(([name, types]) => ({name, types}))
+}
+function listToJsArray(csArr) {
+    let arr = [];
+    if (!csArr) return arr;
+    for (var i = 0; i < csArr.Count; i++) {
+        arr.push(csArr.get_Item(i));
+    }
+    return arr;
 }
