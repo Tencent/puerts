e2df7ac5c8d03e81189a00b598dc7a792d247684
johnche <johnche@tencent.com>
2023-05-04 17:19:02 +0800

    [unreal]补充copy right声明

--- unreal/Puerts/Source/WasmCore/Public/WasmBindingTemplate.hpp
+++ unreal/Puerts/Source/WasmCore/Public/WasmBindingTemplate.hpp
@@ -1,4 +1,12 @@
-﻿#pragma once
+﻿/*
+ * Tencent is pleased to support the open source community by making Puerts available.
+ * Copyright (C) 2020 THL A29 Limited, a Tencent company.  All rights reserved.
+ * Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may
+ * be subject to their corresponding license terms. This file is subject to the terms and conditions defined in file 'LICENSE',
+ * which is part of this source code package.
+ */
+
+#pragma once
 #include "wasm3.h"
 #include "m3_env.h"
 #include "WasmCommonIncludes.h"
