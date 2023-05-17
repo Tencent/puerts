5ad19653d350eec8311b6fc79030820fe7b5f061
johnche <johnche@tencent.com>
2023-05-17 15:00:53 +0800

    [unreal]npm模块，模块目录带扩展名会加载失败的问题（比如ipaddr.js）

--- unreal/Puerts/Source/JsEnv/Private/DefaultJSModuleLoader.cpp
+++ unreal/Puerts/Source/JsEnv/Private/DefaultJSModuleLoader.cpp
@@ -69,18 +69,13 @@ bool DefaultJSModuleLoader::SearchModuleInDir(
 {
     FString Extension = FPaths::GetExtension(RequiredModule);
     bool IsJs = Extension == TEXT("js") || Extension == TEXT("mjs") || Extension == TEXT("cjs") || Extension == TEXT("json");
-    if (Extension == TEXT("") || !IsJs)
-    {
-        return SearchModuleWithExtInDir(Dir, RequiredModule + ".js", Path, AbsolutePath) ||
-               SearchModuleWithExtInDir(Dir, RequiredModule + ".mjs", Path, AbsolutePath) ||
-               SearchModuleWithExtInDir(Dir, RequiredModule + ".cjs", Path, AbsolutePath) ||
-               SearchModuleWithExtInDir(Dir, RequiredModule / "package.json", Path, AbsolutePath) ||
-               SearchModuleWithExtInDir(Dir, RequiredModule / "index.js", Path, AbsolutePath);
-    }
-    else
-    {
-        return SearchModuleWithExtInDir(Dir, RequiredModule, Path, AbsolutePath);
-    }
+    if (IsJs && SearchModuleWithExtInDir(Dir, RequiredModule, Path, AbsolutePath))
+        return true;
+    return SearchModuleWithExtInDir(Dir, RequiredModule + ".js", Path, AbsolutePath) ||
+           SearchModuleWithExtInDir(Dir, RequiredModule + ".mjs", Path, AbsolutePath) ||
+           SearchModuleWithExtInDir(Dir, RequiredModule + ".cjs", Path, AbsolutePath) ||
+           SearchModuleWithExtInDir(Dir, RequiredModule / "package.json", Path, AbsolutePath) ||
+           SearchModuleWithExtInDir(Dir, RequiredModule / "index.js", Path, AbsolutePath);
 }
 
 bool DefaultJSModuleLoader::SearchModuleWithExtInDir(
