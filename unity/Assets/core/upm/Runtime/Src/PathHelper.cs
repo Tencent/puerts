992fc1f0cad8f72dd1f7291eb7d4cf2134a777b3
zombieyang(杨浩) <zombieyang@tencent.com>
2023-06-13 11:03:56 +0800

    Revert "[unity] always enable default mode wrapper in editor"
    This reverts commit 93d2a7117dbea1773d11ce19b2230184ec2a91ec.

--- unity/Assets/core/upm/Runtime/Src/PathHelper.cs
+++ unity/Assets/core/upm/Runtime/Src/PathHelper.cs
@@ -204,5 +204,175 @@ namespace Puerts
 
             return path;
         }
+
+        
+        internal static string JSCode = @"
+    var global = this;
+    (function() {
+        /* eslint-disable max-depth, max-statements, complexity, max-lines-per-function */
+        const SLASH = 47
+        const DOT = 46
+
+        const assertPath = (path) => {
+            const t = typeof path
+            if (t !== 'string') {
+                throw new TypeError(`Expected a string, got a ${t}`)
+            }
+        }
+
+        // this function is directly from node source
+        const posixNormalize = (path, allowAboveRoot) => {
+            let res = ''
+            let lastSegmentLength = 0
+            let lastSlash = -1
+            let dots = 0
+            let code
+
+            for (let i = 0; i <= path.length; ++i) {
+                if (i < path.length) {
+                    code = path.charCodeAt(i)
+                } else if (code === SLASH) {
+                    break
+                } else {
+                    code = SLASH
+                }
+                if (code === SLASH) {
+                    if (lastSlash === i - 1 || dots === 1) {
+                        // NOOP
+                    } else if (lastSlash !== i - 1 && dots === 2) {
+                        if (
+                            res.length < 2 ||
+                            lastSegmentLength !== 2 ||
+                            res.charCodeAt(res.length - 1) !== DOT ||
+                            res.charCodeAt(res.length - 2) !== DOT
+                        ) {
+                            if (res.length > 2) {
+                                const lastSlashIndex = res.lastIndexOf('/')
+                                if (lastSlashIndex !== res.length - 1) {
+                                    if (lastSlashIndex === -1) {
+                                        res = ''
+                                        lastSegmentLength = 0
+                                    } else {
+                                        res = res.slice(0, lastSlashIndex)
+                                        lastSegmentLength = res.length - 1 - res.lastIndexOf('/')
+                                    }
+                                    lastSlash = i
+                                    dots = 0
+                                    continue
+                                }
+                            } else if (res.length === 2 || res.length === 1) {
+                                res = ''
+                                lastSegmentLength = 0
+                                lastSlash = i
+                                dots = 0
+                                continue
+                            }
+                        }
+                        if (allowAboveRoot) {
+                            if (res.length > 0) {
+                                res += '/..'
+                            } else {
+                                res = '..'
+                            }
+                            lastSegmentLength = 2
+                        }
+                    } else {
+                        if (res.length > 0) {
+                            res += '/' + path.slice(lastSlash + 1, i)
+                        } else {
+                            res = path.slice(lastSlash + 1, i)
+                        }
+                        lastSegmentLength = i - lastSlash - 1
+                    }
+                    lastSlash = i
+                    dots = 0
+                } else if (code === DOT && dots !== -1) {
+                    ++dots
+                } else {
+                    dots = -1
+                }
+            }
+
+            return res
+        }
+
+        const decode = (s) => {
+            try {
+                return decodeURIComponent(s)
+            } catch {
+                return s
+            }
+        }
+
+        const normalize = (p) => {
+            assertPath(p)
+
+            let path = p
+            if (path.length === 0) {
+                return '.'
+            }
+
+            const isAbsolute = path.charCodeAt(0) === SLASH
+            const trailingSeparator = path.charCodeAt(path.length - 1) === SLASH
+
+            path = decode(path)
+            path = posixNormalize(path, !isAbsolute)
+
+            if (path.length === 0 && !isAbsolute) {
+                path = '.'
+            }
+            if (path.length > 0 && trailingSeparator) {
+                path += '/'
+            }
+            if (isAbsolute) {
+                return '/' + path
+            }
+
+            return path
+        }
+
+        global.__puer_path__ = {
+            normalize: normalize,
+            isAbsolute(filepath) {
+                return !(
+                    !/^[\\\/]{2,}[^\\\/]+[\\\/]+[^\\\/]+/.test(filepath) && 
+                    !/^([a-z]:)?[\\\/]/i.test(filepath)
+                )
+            },
+            isRelative(filepath) {
+                if (filepath[0] == '.') {
+                    if (filepath.length == 1 || filepath[1] == '/') return true;
+                    if (filepath[1] == '.') {
+                        if (filepath.length == 2 || filepath[2] == '/') return true;
+                    }
+                }
+                return false;
+            },
+            dirname: function dirname(path) {
+                if (path.length === 0) return '.';
+                var code = path.charCodeAt(0);
+                var hasRoot = code === 47 /*/*/;
+                var end = -1;
+                var matchedSlash = true;
+                for (var i = path.length - 1; i >= 1; --i) {
+                    code = path.charCodeAt(i);
+                    if (code === 47 /*/*/) {
+                        if (!matchedSlash) {
+                            end = i;
+                            break;
+                        }
+                        } else {
+                        // We saw the first non-path separator
+                        matchedSlash = false;
+                    }
+                }
+
+                if (end === -1) return hasRoot ? '/' : '.';
+                if (hasRoot && end === 1) return '//';
+                return path.slice(0, end);
+            }
+        }
+    })()
+            ";
     }
 }
