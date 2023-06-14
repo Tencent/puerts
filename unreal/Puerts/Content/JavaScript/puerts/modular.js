ecdd01cb939dc8c5652cc78d5ac73fa1bcd56e6b
liangcheng <liangcheng@kingsoft.com>
2023-06-14 16:03:26 +0800

    [unreal] fix: 修复require某些esm模块失败的问题

--- unreal/Puerts/Content/JavaScript/puerts/modular.js
+++ unreal/Puerts/Content/JavaScript/puerts/modular.js
@@ -129,7 +129,12 @@ var global = global || (function () { return this; }());
                     isESM = packageConfigure.type === "module"
                     let url = packageConfigure.main || "index.js";
                     if (isESM) {
-                        url = packageConfigure.exports && packageConfigure.exports["."] && ((packageConfigure.exports["."]["default"] && packageConfigure.exports["."]["default"]["require"]) || (packageConfigure.exports["."]["require"] && packageConfigure.exports["."]["require"]["default"]))
+                        let packageExports = packageConfigure.exports && packageConfigure.exports["."];
+                        if (packageExports)
+                            url =
+                                (packageExports["default"] && packageExports["default"]["require"]) ||
+                                (packageExports["require"] && packageExports["require"]["default"]) ||
+                                packageExports["require"];                        
                         if (!url) {
                             throw new Error("can not require a esm in cjs module!");
                         }
