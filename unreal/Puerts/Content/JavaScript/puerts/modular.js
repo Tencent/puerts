085d821fa18b5c5e59ccde37175c40925fe1e577
PrimaryFantasty <primaryfantasaty@qq.com>
2023-06-27 19:12:03 +0800

    fix: ignore node_modules package bad effect.

--- unreal/Puerts/Content/JavaScript/puerts/modular.js
+++ unreal/Puerts/Content/JavaScript/puerts/modular.js
@@ -120,12 +120,8 @@ var global = global || (function () { return this; }());
             moduleCache[key] = m;
             let sid = addModule(m);
             let script = loadModule(fullPath);
-            isESM = isESM === true || fullPath.endsWith(".mjs")
-            let cachedIsESM = isESM;
-            if (fullPath.endsWith(".cjs")) {
-                isESM = false;
-                cachedIsESM = false;
-            }
+            isESM = (isESM === true && fullPath.includes("/node_modules/")) || fullPath.endsWith(".mjs")
+            if (fullPath.endsWith(".cjs")) isESM = false;
             if (fullPath.endsWith(".json")) {
                 let packageConfigure = JSON.parse(script);
                 
@@ -142,14 +138,12 @@ var global = global || (function () { return this; }());
                         if (!url) {
                             throw new Error("can not require a esm in cjs module!");
                         }
-                        isESM = cachedIsESM;
                     }
                     let fullDirInJs = (fullPath.indexOf('/') != -1) ? fullPath.substring(0, fullPath.lastIndexOf("/")) : fullPath.substring(0, fullPath.lastIndexOf("\\")).replace(/\\/g, '\\\\');
                     let tmpRequire = genRequire(fullDirInJs, isESM);
                     let r = tmpRequire(url);
                     tmpModuleStorage[sid] = undefined;
                     m.exports = r;
-                    isESM = cachedIsESM;
                 } else {
                     tmpModuleStorage[sid] = undefined;
                     m.exports = packageConfigure;
