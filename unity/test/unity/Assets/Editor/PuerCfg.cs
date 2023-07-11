79019812168863a3c4db08d7b50daf15aa434793
zombieyang(杨浩) <zombieyang@tencent.com>
2023-07-11 16:03:56 +0800

    [unity] fix blittablecopy bug and add unittest fix #1427

--- unity/test/unity/Assets/Editor/PuerCfg.cs
+++ unity/test/unity/Assets/Editor/PuerCfg.cs
@@ -8,7 +8,7 @@ public class PuerCfg {
     public static List<Type> binding {
         get {
             return new List<Type>{
-                typeof(UnityEngine.Vector3),
+                typeof(UnityEngine.Color),
                 typeof(Dictionary<int, int>),
                 typeof(Dictionary<int, int>.Enumerator),
                 typeof(List<int>.Enumerator),
@@ -17,6 +17,18 @@ public class PuerCfg {
                 typeof(Puerts.UnitTest.ExtensionTestHelperDerived),
                 typeof(Puerts.UnitTest.ExtensionTestHelper1),
                 typeof(Puerts.UnitTest.ExtensionTestHelperDerived1)
+            };
+        }
+    }
+    [BlittableCopy]
+    static IEnumerable<Type> Blittables
+    {
+        get
+        {
+            return new List<Type>()
+            {
+                //打开这个可以优化Vector3的GC，但需要开启unsafe编译
+                typeof(UnityEngine.Color),
             };
         }
     }
