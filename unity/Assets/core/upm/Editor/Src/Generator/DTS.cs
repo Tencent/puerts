ef451725a2101b26966c92197a3a35c5eff6849d
zombieyang <zombieyang@tencent.com>
2023-09-28 20:48:59 +0800

    [unity] fix #1541

--- unity/Assets/core/upm/Editor/Src/Generator/DTS.cs
+++ unity/Assets/core/upm/Editor/Src/Generator/DTS.cs
@@ -681,8 +681,10 @@ namespace Puerts.Editor
                         }
                     }
 
-                    if (refTypes.Contains(rawType) || type.IsPointer || rawType.IsPointer) return;
-                    if (!rawType.IsGenericParameter)
+                    // if type == rawType, there is a chance that the type is already added to refTypes when it was a rawType.
+                    // so, when refTypes contains rawType but type == rawType, most of the remain logic is still needed to run. 
+                    if ((refTypes.Contains(rawType) && type != rawType) || type.IsPointer || rawType.IsPointer) return;
+                    if (!rawType.IsGenericParameter && !refTypes.Contains(rawType))
                     {
                         refTypes.Add(rawType);
                     }
