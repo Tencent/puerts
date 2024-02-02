632596929065104e03055e5ff25394022eaef004
wusidn <wusidn@163.com>
2024-02-02 16:16:04 +0800

    不必要的Reset导致内存泄露，Release传入的地址是null

--- unreal/Puerts/Source/JsEnv/Private/JsEnvImpl.cpp
+++ unreal/Puerts/Source/JsEnv/Private/JsEnvImpl.cpp
@@ -2604,7 +2604,6 @@ bool FJsEnvImpl::ClearDelegate(v8::Isolate* Isolate, v8::Local<v8::Context>& Con
     if (Iter->second.Proxy.IsValid())
     {
         Iter->second.Proxy->JsFunction.Reset();
-        Iter->second.Proxy.Reset();
         SysObjectRetainer.Release(Iter->second.Proxy.Get());
     }
 
