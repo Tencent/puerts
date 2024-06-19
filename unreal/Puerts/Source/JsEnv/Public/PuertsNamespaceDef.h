4410df740da40467833e92a5dab1199464da0e85
johnche <johnche@tencent.com>
2024-06-19 16:20:50 +0800

    [unity][unreal]PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS实现有误

--- unreal/Puerts/Source/JsEnv/Public/PuertsNamespaceDef.h
+++ unreal/Puerts/Source/JsEnv/Public/PuertsNamespaceDef.h
@@ -17,27 +17,21 @@
 #endif
 
 #ifndef PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS
-    #define PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS \
-		#if defined(_MSC_VER) \
-		    #pragma warning(push) \
-		    #pragma warning(disable : 4668) \
-		#elif defined(__clang__) \
-		    #pragma clang diagnostic push \
-		    #pragma clang diagnostic ignored "-Wundef" \
-		#elif defined(__GNUC__) \
-		    #pragma GCC diagnostic push \
-		    #pragma GCC diagnostic ignored "-Wundef" \
-		#endif
-#endif // PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS
+#if defined(_MSC_VER)
+#define PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS __pragma(warning(push)) __pragma(warning(disable : 4668))
+#elif defined(__clang__)
+#define PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS pragma clang diagnostic push pragma clang diagnostic ignored "-Wundef"
+#elif defined(__GNUC__)
+#define PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS pragma GCC diagnostic push pragma GCC diagnostic ignored "-Wundef"
+#endif
+#endif    // PRAGMA_DISABLE_UNDEFINED_IDENTIFIER_WARNINGS
 
 #ifndef PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS
-	#define PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS \
-		#if defined(_MSC_VER) \
-		    #pragma warning(pop) \
-		#elif defined(__clang__) \
-		    #pragma clang diagnostic pop \
-		#elif defined(__GNUC__) \
-		    #pragma GCC diagnostic pop \
-		#endif
-#endif // PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS
-
+#if defined(_MSC_VER)
+#define PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS __pragma(warning(pop))
+#elif defined(__clang__)
+#define PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS pragma clang diagnostic pop
+#elif defined(__GNUC__)
+#define PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS pragma GCC diagnostic pop
+#endif
+#endif    // PRAGMA_ENABLE_UNDEFINED_IDENTIFIER_WARNINGS
