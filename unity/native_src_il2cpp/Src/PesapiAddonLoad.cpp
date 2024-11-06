0415fc45f9e8d4481b8fbf6bcbae174f49fe867a
johnche <johnche@tencent.com>
2024-11-06 11:10:04 +0800

    [unity]去掉多余代码

--- unity/native_src_il2cpp/Src/PesapiAddonLoad.cpp
+++ unity/native_src_il2cpp/Src/PesapiAddonLoad.cpp
@@ -7,42 +7,11 @@
  */
 
 #include "pesapi.h"
-#if defined(PLATFORM_WINDOWS)
-#include <windows.h>
-#endif
-
-#include <map>
-#include <string>
-#include <iostream>
-#include <regex>
-
-#pragma warning(push, 0)  
-#include "libplatform/libplatform.h"
-#include "v8.h"
-#pragma warning(pop)
-
-#define STRINGIFY_(x) #x
-#define STRINGIFY(x) STRINGIFY_(x)
-
-#ifndef MSVC_PRAGMA
-#if !defined(__clang__) && defined(_MSC_VER)
-	#define MSVC_PRAGMA(Pragma) __pragma(Pragma)
-#else
-	#define MSVC_PRAGMA(...)
-#endif
-#endif
-
-static std::map<std::string, void*> GHandlers;
-
-static int LoadAddon(const char* path, const char* module_name)
-{
-    return -1;
-}
 
 EXTERN_C_START
 int pesapi_load_addon(const char* path, const char* module_name)
 {
-    return LoadAddon(path, module_name);
+    return -1;
 }
 
 EXTERN_C_END
