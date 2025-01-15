dd6797f88c6a7abfe2a23a49386d81a6ebe798b4
morirain <morirain@outlook.com>
2025-01-15 20:29:05 +0800

    [unreal]对于原生cpp绑定，若未绑定构造函数，则在ts声明中加入abstract标记，可以在ts中尝试构造时提示报错
    Signed-off-by: morirain <morirain@outlook.com>

--- unreal/Puerts/Source/DeclarationGenerator/Private/TemplateBindingGenerator.cpp
+++ unreal/Puerts/Source/DeclarationGenerator/Private/TemplateBindingGenerator.cpp
@@ -83,7 +83,17 @@ struct FGenImp
     {
         if (HasUENamespace(ClassDefinition->ScriptName))
             return;
-        Output << "    class " << ClassDefinition->ScriptName;
+
+        Output << "    ";
+
+        PUERTS_NAMESPACE::NamedFunctionInfo* ConstructorInfo = ClassDefinition->ConstructorInfos;
+        const bool IsNoConstructorInfos = !ConstructorInfo || !(ConstructorInfo->Name && ConstructorInfo->Type);
+        if (IsNoConstructorInfos)
+        {
+            Output << "abstract ";
+        }
+
+        Output << "class " << ClassDefinition->ScriptName;
         if (ClassDefinition->SuperTypeId)
         {
             Output << " extends " << PUERTS_NAMESPACE::FindClassByID(ClassDefinition->SuperTypeId)->ScriptName;
@@ -92,7 +102,6 @@ struct FGenImp
 
         TSet<FString> AddedFunctions;
 
-        PUERTS_NAMESPACE::NamedFunctionInfo* ConstructorInfo = ClassDefinition->ConstructorInfos;
         while (ConstructorInfo && ConstructorInfo->Name && ConstructorInfo->Type)
         {
             FStringBuffer Tmp;
