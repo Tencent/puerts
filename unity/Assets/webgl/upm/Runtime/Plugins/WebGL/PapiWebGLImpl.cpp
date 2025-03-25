aba1e040c6f5ebd51186e3685d137f9fcd318935
johnche <johnche@tencent.com>
2025-03-25 11:57:12 +0800

    [unity]一批获取api

--- unity/Assets/webgl/upm/Runtime/Plugins/WebGL/PapiWebGLImpl.cpp
+++ unity/Assets/webgl/upm/Runtime/Plugins/WebGL/PapiWebGLImpl.cpp
@@ -41,6 +41,14 @@ enum {
 #define JS_EXCEPTION JS_MKVAL(JS_TAG_EXCEPTION, 0)
 #define JS_UNINITIALIZED JS_MKVAL(JS_TAG_UNINITIALIZED, 0)
 
+#define JS_VALUE_GET_TAG(v) ((int32_t)(v).tag)
+#define JS_VALUE_GET_INT(v) ((v).u.int32)
+#define JS_VALUE_GET_BOOL(v) ((v).u.int32)
+#define JS_VALUE_GET_FLOAT64(v) ((v).u.float64)
+#define JS_VALUE_GET_PTR(v) ((v).u.ptr)
+
+#define JS_TAG_IS_FLOAT64(tag) ((unsigned)(tag) == JS_TAG_FLOAT64)
+
 typedef union JSValueUnion {
     int32_t int32;
     double float64;
@@ -221,6 +229,22 @@ pesapi_value pesapi_create_generic2(pesapi_env env, T1 v1, T2 v2, Func createFun
     return nullptr;
 }
 
+template<typename T, typename Func>
+T pesapi_get_value_generic(pesapi_env env, pesapi_value pvalue, Func convertFunc)
+{
+    (void)env;
+    T ret = 0;
+    convertFunc(&ret, *(reinterpret_cast<JSValue*>(pvalue)));
+    return ret;
+}
+
+template<typename Func>
+bool pesapi_is_generic(pesapi_env env, pesapi_value pvalue, Func convertFunc)
+{
+	(void)env;
+	return convertFunc(*(reinterpret_cast<JSValue*>(pvalue)));
+}
+
 static inline JSValue JS_NewInt32(int32_t val)
 {
     return JS_MKVAL(JS_TAG_INT, val);
@@ -280,6 +304,140 @@ JSValue JS_NewBufferLen(void *buf, uint32_t buf_len)
     return v;
 }
 
+int JS_ToBool(bool *pres, JSValue val)
+{
+    int32_t tag = JS_VALUE_GET_TAG(val);
+    bool ret;
+    switch(tag) {
+    case JS_TAG_INT:
+        ret = JS_VALUE_GET_INT(val) != 0;
+        break;
+    case JS_TAG_BOOL:
+    case JS_TAG_NULL:
+    case JS_TAG_UNDEFINED:
+        ret = JS_VALUE_GET_INT(val);
+        break;
+    case JS_TAG_STRING:
+        {
+            ret = val.len != 0;
+        }
+        break;
+    case JS_TAG_INT64:
+    case JS_TAG_UINT64:
+        {
+            ret = val.u.int64 != 0;
+        }
+        break;
+    default:
+        ret = false;
+    }
+    *pres = ret;
+    return 0;
+}
+
+typedef union JSFloat64Union {
+    double d;
+    uint64_t u64;
+    uint32_t u32[2];
+} JSFloat64Union;
+
+#  define likely(x)       (x)
+
+static int JS_ToInt32(int32_t *pres, JSValue val)
+{
+    uint32_t tag;
+    int32_t ret;
+
+    tag = JS_VALUE_GET_TAG(val);
+    switch(tag) {
+    case JS_TAG_INT:
+    case JS_TAG_BOOL:
+    case JS_TAG_NULL:
+    case JS_TAG_UNDEFINED:
+        ret = JS_VALUE_GET_INT(val);
+        break;
+    case JS_TAG_FLOAT64:
+        {
+            JSFloat64Union u;
+            double d;
+            int e;
+            d = JS_VALUE_GET_FLOAT64(val);
+            u.d = d;
+            /* we avoid doing fmod(x, 2^32) */
+            e = (u.u64 >> 52) & 0x7ff;
+            if (likely(e <= (1023 + 30))) {
+                /* fast case */
+                ret = (int32_t)d;
+            } else if (e <= (1023 + 30 + 53)) {
+                uint64_t v;
+                /* remainder modulo 2^32 */
+                v = (u.u64 & (((uint64_t)1 << 52) - 1)) | ((uint64_t)1 << 52);
+                v = v << ((e - 1023) - 52 + 32);
+                ret = v >> 32;
+                /* take the sign into account */
+                if (u.u64 >> 63)
+                    if (ret != INT32_MIN)
+                        ret = -ret;
+            } else {
+                ret = 0; /* also handles NaN and +inf */
+            }
+        }
+        break;
+        
+    case JS_TAG_INT64:
+    case JS_TAG_UINT64:
+        {
+            ret = (int)val.u.int64;
+        }
+        break;
+    default:
+        return -1;
+    }
+    *pres = ret;
+    return 0;
+}
+
+static inline int JS_ToUint32(uint32_t *pres, JSValue val)
+{
+    return JS_ToInt32((int32_t*)pres, val);
+}
+
+static inline int JS_ToInt64(int64_t *pres, JSValue val)
+{
+    uint32_t tag = JS_VALUE_GET_TAG(val);
+    if (tag == JS_TAG_INT64 || tag == JS_TAG_UINT64) {
+        *pres = val.u.int64;
+        return 0;
+    } else {
+        return -1;
+    }
+}
+
+static inline int JS_ToUint64(uint64_t *pres, JSValue val)
+{
+    return JS_ToInt64((int64_t*)pres, val);
+}
+
+static inline int JS_ToFloat64(double *pres, JSValue val)
+{
+    uint32_t tag;
+
+    tag = JS_VALUE_GET_TAG(val);
+    if (tag <= JS_TAG_NULL) {
+        *pres = JS_VALUE_GET_INT(val);
+        return 0;
+    } else if (JS_TAG_IS_FLOAT64(tag)) {
+        *pres = JS_VALUE_GET_FLOAT64(val);
+        return 0;
+    } else if (tag == JS_TAG_INT64) {
+        *pres = val.u.int64;
+    } else if (tag == JS_TAG_UINT64) {
+        *pres = val.u.uint64;
+    } else {
+        return -1;
+    }
+}
+
 // value process
 pesapi_value pesapi_create_null(pesapi_env env)
 {
@@ -329,6 +487,57 @@ pesapi_value pesapi_create_string_utf8(pesapi_env env, const char *str, size_t l
 pesapi_value pesapi_create_binary(pesapi_env env, void *bin, size_t length)
 {
     return pesapi_create_generic2(env, bin, length, JS_NewBufferLen);
+}
+
+pesapi_value pesapi_create_array(pesapi_env env)
+{
+    return {};
+}
+
+// js那处理，返回index，然后存在JSValue返回
+pesapi_value pesapi_create_object(pesapi_env env)
+{
+    return {};
+}
+
+pesapi_value pesapi_create_function(pesapi_env env, pesapi_callback native_impl, void* data, pesapi_function_finalize finalize)
+{
+    return {};
+}
+
+pesapi_value pesapi_create_class(pesapi_env env, const void* type_id)
+{
+    return {};
+}
+
+bool pesapi_get_value_bool(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<bool>(env, pvalue, JS_ToBool);
+}
+
+int32_t pesapi_get_value_int32(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<int32_t>(env, pvalue, JS_ToInt32);
+}
+
+uint32_t pesapi_get_value_uint32(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<uint32_t>(env, pvalue, JS_ToUint32);
+}
+
+int64_t pesapi_get_value_int64(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<int64_t>(env, pvalue, JS_ToInt64);
+}
+
+uint64_t pesapi_get_value_uint64(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<uint64_t>(env, pvalue, JS_ToUint64);
+}
+
+double pesapi_get_value_double(pesapi_env env, pesapi_value pvalue)
+{
+    return pesapi_get_value_generic<double>(env, pvalue, JS_ToFloat64);
 }
 
 pesapi_open_scope_func g_js_open_scope = nullptr;
