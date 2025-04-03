f0d9478627452e8e0c7bac6e5571240869b805f2
johnche <johnche@tencent.com>
2025-04-03 10:29:57 +0800

    UNITY_WEBGL下JsEnv不能new多个实例

--- unity/Assets/core/upm/Runtime/Src/IL2Cpp/JsEnv.cs
+++ unity/Assets/core/upm/Runtime/Src/IL2Cpp/JsEnv.cs
@@ -59,7 +59,7 @@ namespace Puerts
         public JsEnv(ILoader loader, int debugPort = -1, BackendType backend = BackendType.Auto, IntPtr externalRuntime = default(IntPtr), IntPtr externalContext = default(IntPtr))
         {
             this.loader = loader;
-            
+            disposed = true;
             if (!isInitialized)
             {
                 lock (jsEnvs)
@@ -82,8 +82,13 @@ namespace Puerts
                     }
                 }
             }
+#if UNITY_WEBGL
+            else
+            {
+                throw new InvalidOperationException("more than one JsEnv instance is not supported in WebGL");
+            }
+#endif
 
-            disposed = true;
             nativeJsEnv = Puerts.PuertsDLL.CreateJSEngine((int)backend);
             if (nativeJsEnv == IntPtr.Zero)
             {
