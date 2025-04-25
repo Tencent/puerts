0512d2c5c6d73986bf3a2a85f525baebde5d5b87
johnche <johnche@tencent.com>
2025-04-25 17:25:28 +0800

    [unity]把webgl-jsbuild的功能移植过来，做两点优化：1、Editor下不依赖nodejs  backend；2、放在PostBuild自动化做js资源的合并和拷贝

---
+++ unity/Assets/webgl/upm/Cli/Javascripts~/index.js
@@ -0,0 +1,19 @@
+const fs = require('fs');
+const cp = require('child_process');
+
+if (!fs.existsSync(__dirname + 'node_modules')) {
+  cp.exec('npm i', { cwd: __dirname }, (error, stdout, stderr) => {
+    if (error) {
+      console.error('Error in installing dependency ', error);
+    } else {
+      console.log('Success');
+      executeCommand();
+    }
+  });
+} else {
+  executeCommand();
+}
+
+function executeCommand () {
+    require('./cmd.js');
+};
