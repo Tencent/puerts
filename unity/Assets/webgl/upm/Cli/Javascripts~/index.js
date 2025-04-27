203beb97979f5952dc829b89f1069a7386af2c73
AlexLam1989 <ejay42654671@qq.com>
2025-04-27 19:09:14 +0800

    Update index.js
    windows下会重复npm i安装，改用path.join拼接node_modules文件夹路径

--- unity/Assets/webgl/upm/Cli/Javascripts~/index.js
+++ unity/Assets/webgl/upm/Cli/Javascripts~/index.js
@@ -1,7 +1,8 @@
 const fs = require('fs');
 const cp = require('child_process');
+const path = require('path');
 
-if (!fs.existsSync(__dirname + 'node_modules')) {
+if (!fs.existsSync(path.join(__dirname, 'node_modules'))) {
   cp.exec('npm i', { cwd: __dirname }, (error, stdout, stderr) => {
     if (error) {
       console.error('Error in installing dependency ', error);
