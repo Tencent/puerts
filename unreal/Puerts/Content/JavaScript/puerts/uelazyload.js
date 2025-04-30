258e09d2347107bc010d461dbf0daf91648c4d93
johnche <johnche@tencent.com>
2025-04-30 12:30:33 +0800

    [unreal]把Object.hasOwn换成兼容性更好的Object.hasOwnProperty fix https://github.com/Tencent/puerts/issues/2001#issuecomment-2838978951

--- unreal/Puerts/Content/JavaScript/puerts/uelazyload.js
+++ unreal/Puerts/Content/JavaScript/puerts/uelazyload.js
@@ -302,7 +302,7 @@ var global = global || (function () { return this; }());
     
     function translateType(t) {
         if (typeof t !== 'number') {
-            if (Object.hasOwn(t, '__puerts_ufield')) {
+            if (Object.hasOwnProperty(t, '__puerts_ufield')) {
                 return t.__puerts_ufield
             } else {
                 return t.StaticClass();
