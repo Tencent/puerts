ea3d8ac373515c77d9406c03b7f4941380d93503
johnche <johnche@tencent.com>
2025-06-23 14:54:41 +0800

    lua代码

---
+++ unity/native/papi-lua/make_linux64.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+CONFIG=${1:-Release}
+
+mkdir -p build_linux64 && cd build_linux64
+cmake -DCMAKE_VERBOSE_MAKEFILE:BOOL=ON -DCMAKE_BUILD_TYPE=$CONFIG ..
+cd ..
+cmake --build build_linux64  --config $CONFIG
