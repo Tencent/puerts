336b4d034e20131779bc6b60e5363f26456ebd59
johnche <johnche@tencent.com>
2025-06-25 09:55:29 +0800

    添加api的实现

--- unity/native/papi-lua/source/PapiExport.cpp
+++ unity/native/papi-lua/source/PapiExport.cpp
@@ -23,11 +23,29 @@ PESAPI_MODULE_EXPORT pesapi_ffi* GetLuaFFIApi()
 
 PESAPI_MODULE_EXPORT pesapi_env_ref CreateLuaPapiEnvRef()
 {
-    return {};
+    lua_State* L = luaL_newstate();
+    luaL_openlibs(L);
+    auto mapper = (pesapi::luaimpl::CppObjectMapper*)malloc(sizeof(pesapi::luaimpl::CppObjectMapper));
+    new (mapper) pesapi::luaimpl::CppObjectMapper();
+    mapper->Initialize(L);
+    auto env = reinterpret_cast<pesapi_env>(L);
+    return pesapi::luaimpl::g_pesapi_ffi.create_env_ref(env);
 }
 
-PESAPI_MODULE_EXPORT void DestroyQjsPapiEnvRef(pesapi_env_ref env_ref)
+PESAPI_MODULE_EXPORT void DestroyLuaPapiEnvRef(pesapi_env_ref env_ref)
 {
+    auto scope = pesapi::luaimpl::g_pesapi_ffi.open_scope(env_ref);
+    auto L = reinterpret_cast<lua_State*>(pesapi::luaimpl::g_pesapi_ffi.get_env_from_ref(env_ref));
+    pesapi::luaimpl::CppObjectMapper* mapper = pesapi::luaimpl::CppObjectMapper::Get(L);
+    pesapi::luaimpl::g_pesapi_ffi.close_scope(scope);
+    pesapi::luaimpl::g_pesapi_ffi.release_env_ref(env_ref);
+    mapper->UnInitialize(L);
+    lua_close(L);
+    if (mapper)
+    {
+        mapper->~CppObjectMapper();
+        free(mapper);
+    }
 }
 
 #ifdef __cplusplus
