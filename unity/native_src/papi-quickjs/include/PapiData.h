981ae297fc72b5656d43749d199fb378eaf656e1
corejiang <corejiang@tencent.com>
2025-07-01 10:44:23 +0800

    [unity]fix: 移除quickjs papi对quickjs-atom.h的依赖

--- unity/native_src/papi-quickjs/include/PapiData.h
+++ unity/native_src/papi-quickjs/include/PapiData.h
@@ -7,15 +7,6 @@
 #include <vector>
 #include "CppObjectMapperQuickjs.h"
 
-enum
-{
-    JS_ATOM_NULL_,
-#define DEF(name, str) JS_ATOM_##name,
-#include "quickjs-atom.h"
-#undef DEF
-    JS_ATOM_END,
-};
-
 namespace pesapi
 {
 namespace qjsimpl
