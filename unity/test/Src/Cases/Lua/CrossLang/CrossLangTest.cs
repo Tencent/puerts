8adf56cfcd8ed92bf0534369d4ba194340c38334
johnche <johnche@tencent.com>
2025-07-09 11:24:13 +0800

    lua用例调整得和原js用例尽量形式一致

--- unity/test/Src/Cases/Lua/CrossLang/CrossLangTest.cs
+++ unity/test/Src/Cases/Lua/CrossLang/CrossLangTest.cs
@@ -13,18 +13,19 @@ namespace Puerts.UnitTest
             var luaEnv = new ScriptEnv(new BackendLua());
             luaEnv.Eval(@"
                 local TestHelper = CS.Puerts.UnitTest.TestHelper
+                local assertAndPrint = TestHelper.AssertAndPrint
                 local testHelper = TestHelper.GetInstance()
                 local outRef = {}
                 local oAB = buffer({1})
 
                 local rAB = testHelper:ArrayBufferTestPipeLine(oAB, outRef, function(bi)
-                    TestHelper.AssertAndPrint('LuaGetArrayBufferArgFromCS', bi[1], 2)
+                    assertAndPrint('LuaGetArrayBufferArgFromCS', bi[1], 2)
                     local res = buffer({3})
                     return res
                 end)
                 
-                TestHelper.AssertAndPrint('LuaGetArrayBufferOutArgFromCS', outRef[1][1], 4)
-                TestHelper.AssertAndPrint('JSGetArrayBufferReturnFromCS', rAB[1], 5)
+                assertAndPrint('LuaGetArrayBufferOutArgFromCS', outRef[1][1], 4)
+                assertAndPrint('JSGetArrayBufferReturnFromCS', rAB[1], 5)
 
                 testHelper.arrayBufferTestField = buffer({192})
                 testHelper.arrayBufferTestProp = buffer({192})
@@ -32,7 +33,7 @@ namespace Puerts.UnitTest
                 TestHelper.arrayBufferTestPropStatic = buffer({192})
                 local tmp = TestHelper.arrayBufferTestPropStatic
                 testHelper:ArrayBufferTestCheckMemberValue();
-                TestHelper.AssertAndPrint('LuaArrayBufferShouldBeCopied', tmp[1], 192);
+                assertAndPrint('LuaArrayBufferShouldBeCopied', tmp[1], 192);
             ");
         }
     }
