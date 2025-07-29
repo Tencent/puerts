bfede83f4af135054be9fd6332125356459f4813
johnche <johnche@tencent.com>
2025-07-29 09:27:23 +0800

    LuaLoader加名字空间

--- unity/Assets/core/upm/Runtime/Src/Loaders/LuaLoader.cs
+++ unity/Assets/core/upm/Runtime/Src/Loaders/LuaLoader.cs
@@ -7,37 +7,41 @@
 
 using System.IO;
 
-public interface LuaLoader
+namespace Puerts
 {
-    // string or ArrrayBuffer
-    object ReadFile(string filepath, out string debugpath);
-}
 
-public class LuaDefaultLoader : LuaLoader
-{
+    public interface LuaLoader
+    {
+        // string or ArrrayBuffer
+        object ReadFile(string filepath, out string debugpath);
+    }
+
+    public class LuaDefaultLoader : LuaLoader
+    {
 #if ENABLE_IL2CPP
     [UnityEngine.Scripting.Preserve]
 #endif
-    public virtual object ReadFile(string filepath, out string debugpath)
-    {
-        filepath = filepath.Replace('.', '/');
-        debugpath = filepath;
-        if (!debugpath.EndsWith(".lua"))
+        public virtual object ReadFile(string filepath, out string debugpath)
         {
-            debugpath += ".lua";
-        }
-        try
-        {
+            filepath = filepath.Replace('.', '/');
+            debugpath = filepath;
+            if (!debugpath.EndsWith(".lua"))
+            {
+                debugpath += ".lua";
+            }
+            try
+            {
 #if PUERTS_GENERAL
             return File.ReadAllText(debugpath);
 #else
-            UnityEngine.TextAsset file = (UnityEngine.TextAsset)UnityEngine.Resources.Load(filepath);
-            return (file == null) ? null : file.text;
+                UnityEngine.TextAsset file = (UnityEngine.TextAsset)UnityEngine.Resources.Load(filepath);
+                return (file == null) ? null : file.text;
 #endif
-        }
-        catch
-        {
-            return null;
+            }
+            catch
+            {
+                return null;
+            }
         }
     }
 }
