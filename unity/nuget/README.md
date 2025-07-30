86118ee943e0b018eaaa4674e6ebdc2917c4bfa2
realybin <realybin@outlook.com>
2025-07-30 09:47:09 +0800

    nuget 自动打包和发布 (#2154)

---
+++ unity/nuget/README.md
@@ -0,0 +1,99 @@
+# Nuget
+
+## 使用
+
+可用的平台 `.net8.0`, `win-x64`, `linux-x64`, `osx-x64`, `osx-arm64`
+
+目前不支持`AOT`
+
+引入`Puerts` ( `Puerts.Core` 包)，并根据需求引入需要运行的平台的 `NativeAssets` (.dll, .so ...)，
+没有 `NativeAssets` 则无法在对应的平台运行
+
+例如
+
+* `Puerts.Core.NativeAssets.Win32` 对应 `Windows`
+* `Puerts.Core.NativeAssets.Linux` 对应 `Linux`
+* `Puerts.Core.NativeAssets.macOS` 对应 `macOS`
+
+引入需要的扩展，以下是支持的扩展，同样的，也需要引入对应的 `NativeAssets`
+
+* `Puerts.Lua`
+* `Puerts.QuickJS`
+* `Puerts.NodeJS`
+* `Puerts.V8`
+
+简单用例，已引入 `Puerts.Core`、 `Puerts.V8` 以及对应的 `NativeAssets` 包
+
+```csharp
+using Puerts;
+
+var jsEnv = new ScriptEnv(
+    new BackendV8());
+    jsEnv.Eval(@"
+            CS.System.Console.WriteLine('hello world');
+        ");
+    jsEnv.Dispose();
+
+    Console.WriteLine("Press any key to exit...");
+```
+
+## 包版本定义
+
+[Directory.Build.props](Directory.Build.props)
+
+构建包的版本
+
+```xml
+<BuildPackageVersion>0.0.1</BuildPackageVersion>
+
+<PuertsCoreVersion>$(BuildPackageVersion)</PuertsCoreVersion>
+<PuertsQuickJsVersion>$(BuildPackageVersion)</PuertsQuickJsVersion>
+<PuertsNodeJsVersion>$(BuildPackageVersion)</PuertsNodeJsVersion>
+<PuertsV8Version>$(BuildPackageVersion)</PuertsV8Version>
+<PuertsLuaVersion>$(BuildPackageVersion)</PuertsLuaVersion>
+```
+
+设置目标的框架
+
+```xml
+<PuertsCurrentTargetFrameworks>net8.0</PuertsCurrentTargetFrameworks>
+<PuertsNativeAssetsLinuxTargetFrameworks>net8.0</PuertsNativeAssetsLinuxTargetFrameworks>
+<PuertsNativeAssetsWin32TargetFrameworks>net8.0</PuertsNativeAssetsWin32TargetFrameworks>
+<PuertsNativeAssetsmacOSTargetFramework>net8.0</PuertsNativeAssetsmacOSTargetFramework>
+<PuertsCurrentTargetmacOSPlatformVersion>14.0</PuertsCurrentTargetmacOSPlatformVersion>
+```
+
+## 环境变量
+
+```powershell
+$env:NUGET_SOURCE="https://example.com/v3/index.json"
+$env:NUGET_PUB_KEY="key"
+```
+
+`NUGET_SOURCE` 打包发布的目标源 - 在指定 `UploadPackage` 后必须设置
+
+`NUGET_PUB_KEY` 目标源的密钥 - 在指定 `UploadPackage` 后必须设置
+
+## 构建
+
+在Windows下构建
+
+```powershell
+.\build.ps1 --NativeAssetsDirectory "xxx\puerts\unity\downloaded_natives" --ProjectsRoot "xxx\puerts\unity\nuget" --UploadPackage $true
+```
+
+在Linux/macOS下构建
+
+```shell
+.\build.sh --NativeAssetsDirectory "xxx\puerts\unity\downloaded_natives" --ProjectsRoot "xxx\puerts\unity\nuget" --UploadPackage "true"
+```
+
+### 构建参数
+
+* `NativeAssetsDirectory` 原生文件目录，放置构建完成的 `native` 项目产物
+* `ProjectsRoot` nuget项目目录
+* `UploadPackage` 是否在构建完包之后上传到指定的 `NUGET_SOURCE`
+
+### 定义构建
+
+构建脚本 [Program.cs](build/Program.cs)
---
+++ unity/nuget/build/Program.cs
@@ -0,0 +1,336 @@
+/*
+* Tencent is pleased to support the open source community by making Puerts available.
+* Copyright (C) 2020 Tencent.  All rights reserved.
+* Puerts is licensed under the BSD 3-Clause License, except for the third-party components listed in the file 'LICENSE' which may be subject to their corresponding license terms. 
+* This file is subject to the terms and conditions defined in file 'LICENSE', which is part of this source code package.
+*/
+using Cake.Common;
+using Cake.Common.IO;
+using Cake.Common.Tools.DotNet;
+using Cake.Common.Tools.DotNet.Pack;
+using Cake.Core;
+using Cake.Core.Diagnostics;
+using Cake.Core.IO;
+using Cake.Frosting;
+using System;
+using System.IO;
+using System.Linq;
+
+public static class Program
+{
+    public static int Main(string[] args)
+    {
+        return new CakeHost()
+            .UseContext<BuildContext>()
+            .Run(args);
+    }
+}
+
+public class BuildContext : FrostingContext
+{
+    /// <summary>
+    /// The full path to the directory containing native assets.
+    /// </summary>
+    public IDirectory NativeAssetsDirectory => this.HasArgument("NativeAssetsDirectory")
+        ? FileSystem.GetDirectory(new DirectoryPath(Arguments.GetArgument("NativeAssetsDirectory")))
+        : throw new CakeException($"Argument '{nameof(NativeAssetsDirectory)}' is required.");
+    /// <summary>
+    /// The full path to the root directory of the projects.
+    /// </summary>
+    public IDirectory ProjectsRoot => this.HasArgument("ProjectsRoot")
+        ? FileSystem.GetDirectory(new DirectoryPath(Arguments.GetArgument("ProjectsRoot")))
+        : throw new CakeException($"Argument '{nameof(ProjectsRoot)}' is required.");
+    public bool IsUploadPackageEnabled => this.HasArgument("UploadPackage")
+        && Arguments.GetArgument("UploadPackage").Equals("true", StringComparison.OrdinalIgnoreCase);
+    public string NUGET_SOURCE => this.HasEnvironmentVariable("NUGET_SOURCE")
+        ? Environment.GetEnvironmentVariable("NUGET_SOURCE")
+        : throw new CakeException("NUGET_SOURCE is not defined in environment variables");
+    public string NUGET_PUB_KEY
+    {
+        get
+        {
+            Log.Warning("Accessing Secret: {0}", nameof(NUGET_PUB_KEY));
+            return this.HasEnvironmentVariable("NUGET_PUB_KEY")
+                ? Environment.GetEnvironmentVariable("NUGET_PUB_KEY")
+                : throw new CakeException("NUGET_PUB_KEY is not defined in environment variables");
+        }
+    }
+    public BuildContext(ICakeContext context)
+        : base(context)
+    {
+    }
+
+    [TaskName("CollectNativeAssets")]
+    public sealed class CollectNativeAssetsTask : FrostingTask<BuildContext>
+    {
+        public override void Run(BuildContext context)
+        {
+            /* .NET RID catalog: https://learn.microsoft.com/en-us/dotnet/core/rid-catalog
+             * 
+             * Excepted Directory structure:
+             * Root\ (NativeAssetsDirectory)
+             *  natives-linux-x64\papi-lua\build_linux_x64_papi-lua\*.so
+             *  natives-linux-x64\papi-quickjs\build_linux_x64_papi-quickjs\*.so
+             *  ...
+             *  natives-win-x64\native\papi-lua\build_win_x64_papi-lua\*.dll
+             *  ...
+             *  natives-osx-x64\native\papi-lua\build_osx_x64_papi-lua\*.dylib
+             *  natives-osx-arm64\native\papi-lua\build_osx_arm64_papi-lua\*.dylib
+             *  ...
+             *  natives-[cmake-rid]\native\[backend-name]\build_[cmake-rid]_*\Release\*.[dll|so|dylib]
+             *  natives-[cmake-rid]\native\[backend-name]\build_[cmake-rid]_*\*.[dll|so|dylib]
+             */
+
+            var nativeAssetsDirectory = context.NativeAssetsDirectory;
+
+            /* Move native assets to the target directory
+             * Puerts.Core.NativeAssets.Linux\lib\
+             *                                    [RID]\native\lib.so
+             */
+
+
+            foreach (var projectItem in WellKnownProjects.NativeAssetsProjects)
+            {
+                if (projectItem.CmakeRid == "osx")
+                {
+                    // natives-osx-auto\Assets\core\upm\Plugins\macOS\*.dylib
+                    var nativeAssetsPathmacOS = nativeAssetsDirectory.Path
+                        .Combine("natives-osx-auto")
+                        .Combine("Assets")
+                        .Combine("core")
+                        .Combine("upm")
+                        .Combine("Plugins")
+                        .Combine("macOS");
+
+                    var targetDirectorymacOS = context.ProjectsRoot.Path
+                        .Combine(projectItem.Name)
+                        .Combine("lib")
+                        .Combine(projectItem.DotNetRid).Combine("native");
+
+                    Directory.CreateDirectory(targetDirectorymacOS.FullPath);
+
+                    var filesmacOS = context.GetFiles(new GlobPattern($"{nativeAssetsPathmacOS.FullPath}/*{projectItem.DotNetNativeName}*dylib"), new GlobberSettings() { IsCaseSensitive = false});
+                    if (filesmacOS.Count == 0)
+                    {
+                        throw new CakeException($"No native assets found in '{nativeAssetsPathmacOS.FullPath}' for project '{projectItem.Name}'.");
+                    }
+                    if (filesmacOS.Count > 1)
+                    {
+                        throw new CakeException($"Multiple native assets found in '{nativeAssetsPathmacOS.FullPath}' for project '{projectItem.Name}'. Expected only one. Found: {filesmacOS.Count}, files: {string.Join(", ", filesmacOS.Select(f => f.FullPath))}");
+                    }
+                    context.CopyFiles(filesmacOS, targetDirectorymacOS.FullPath);
+
+                    continue;
+                }
+
+                var nativeAssetsPath = nativeAssetsDirectory.Path
+                    .Combine("natives-" + projectItem.CmakeRid)
+                    .Combine("native")
+                    .Combine(projectItem.CmakeNativeName);
+
+                if (!Directory.Exists(nativeAssetsPath.FullPath))
+                {
+                    throw new CakeException($"Native assets directory '{nativeAssetsPath.FullPath}' does not exist.");
+                }
+
+                var targetDirectory = context.ProjectsRoot.Path
+                    .Combine(projectItem.Name)
+                    .Combine("lib")
+                    .Combine(projectItem.DotNetRid).Combine("native");
+
+                Directory.CreateDirectory(targetDirectory.FullPath);
+
+                var files = context.GetFiles(new GlobPattern($"{nativeAssetsPath.FullPath}/**/*.*"));
+                context.CopyFiles(files, targetDirectory.FullPath);
+            }
+        }
+
+        [TaskName("PackNugetPackages")]
+        public sealed class PackNugetPackagesTask : FrostingTask<BuildContext>
+        {
+            public override void Run(BuildContext context)
+            {
+                var packageOutputDirectory = context.ProjectsRoot.Path.Combine("packageOutput");
+
+                if (Directory.Exists(packageOutputDirectory.FullPath))
+                {
+                    context.Log.Information($"Cleaning up existing package output directory: {packageOutputDirectory.FullPath}");
+                    Directory.Delete(packageOutputDirectory.FullPath, true);
+                }
+
+                foreach (var coreProject in WellKnownProjects.CoreProjects)
+                {
+                    var csprojPath = context.ProjectsRoot.Path.Combine(coreProject).CombineWithFilePath(coreProject + ".csproj");
+                    context.Log.Information($"Packing NuGet package for project: {coreProject} at {csprojPath}");
+                    context.DotNetPack(csprojPath.FullPath, new DotNetPackSettings()
+                    {
+                        Configuration = "Release",
+                        OutputDirectory = packageOutputDirectory,
+                    });
+                    context.Log.Information($"NuGet package for {coreProject} packed successfully.");
+                }
+
+                foreach (var projectItem in WellKnownProjects.NativeAssetsProjects)
+                {
+                    var csprojPath = context.ProjectsRoot.Path.Combine(projectItem.Name).CombineWithFilePath(projectItem.Name + ".csproj");
+                    context.Log.Information($"Packing NuGet package for project: {projectItem.Name} at {csprojPath}");
+
+                    context.DotNetPack(csprojPath.FullPath, new DotNetPackSettings()
+                    {
+                        Configuration = "Release",
+                        OutputDirectory = packageOutputDirectory,
+                    });
+
+                    context.Log.Information($"NuGet package for {projectItem.Name} packed successfully.");
+                }
+            }
+        }
+
+        [TaskName("UploadNugetPackages")]
+        [IsDependentOn(typeof(PackNugetPackagesTask))]
+        public sealed class UploadNugetPackagesTask : FrostingTask<BuildContext>
+        {
+            public override void Run(BuildContext context)
+            {
+                if (!context.IsUploadPackageEnabled)
+                {
+                    context.Log.Information("Package upload is disabled. Skipping upload.");
+                    return;
+                }
+                var packageOutputDirectory = context.ProjectsRoot.Path.Combine("packageOutput");
+                if (!Directory.Exists(packageOutputDirectory.FullPath))
+                {
+                    throw new DirectoryNotFoundException($"Package output directory '{packageOutputDirectory.FullPath}' does not exist.");
+                }
+
+                var packageFiles = Directory.EnumerateFiles(packageOutputDirectory.FullPath)
+                    .Where(file => file.EndsWith(".nupkg", StringComparison.OrdinalIgnoreCase)
+                                   || file.EndsWith(".snupkg", StringComparison.OrdinalIgnoreCase));
+
+
+                foreach (var nugetPackageFile in packageFiles)
+                {
+                    context.Log.Information($"Uploading NuGet package: {nugetPackageFile}");
+
+                    context.DotNetNuGetPush(
+                        new FilePath(nugetPackageFile),
+                        new Cake.Common.Tools.DotNet.NuGet.Push.DotNetNuGetPushSettings()
+                        {
+                            Source = context.NUGET_SOURCE,
+                            ApiKey = context.NUGET_PUB_KEY
+                        });
+                }
+            }
+        }
+
+
+        [TaskName("Default")]
+        [IsDependentOn(typeof(CollectNativeAssetsTask))]
+        [IsDependentOn(typeof(PackNugetPackagesTask))]
+        [IsDependentOn(typeof(UploadNugetPackagesTask))]
+        public sealed class DefaultTask : FrostingTask<BuildContext>
+        {
+            public override void Run(BuildContext context)
+            {
+                context.Log.Information("Build and packaging completed successfully.");
+                context.Log.Information("Native assets have been collected and NuGet packages have been created.");
+                if (context.IsUploadPackageEnabled)
+                {
+                    context.Log.Information("NuGet packages have been uploaded to the specified source.");
+                }
+                else
+                {
+                    context.Log.Information("Package upload is disabled. No packages were uploaded.");
+                }
+            }
+        }
+
+
+
+        public static class WellKnownProjects
+        {
+            public static readonly string[] CoreProjects =
+            {
+                "Puerts.Core",
+                "Puerts.Lua",
+                "Puerts.NodeJS",
+                "Puerts.QuickJS",
+                "Puerts.V8"
+            };
+
+            /// <summary>
+            /// This array contains the names and RIDs of well-known native projects.
+            /// </summary>
+            /// <remarks>
+            /// This also needs to be defined in their csproj file.
+            /// </remarks>
+            public static readonly ProjectAndRidWithNativeName[] NativeAssetsProjects =
+                [
+                new ProjectAndRidWithNativeName { Name = "Puerts.Core.NativeAssets.Win32", DotNetRid ="win-x64", DotNetNativeName = "Core" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.Core.NativeAssets.Linux", DotNetRid = "linux-x64", DotNetNativeName = "Core" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.Core.NativeAssets.macOS", DotNetRid = "osx", DotNetNativeName = "Core" },
+
+                new ProjectAndRidWithNativeName { Name = "Puerts.Lua.NativeAssets.Win32", DotNetRid = "win-x64", DotNetNativeName = "Lua" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.Lua.NativeAssets.Linux", DotNetRid = "linux-x64", DotNetNativeName = "Lua" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.Lua.NativeAssets.macOS", DotNetRid = "osx", DotNetNativeName = "Lua" },
+
+                new ProjectAndRidWithNativeName { Name = "Puerts.NodeJS.NativeAssets.Win32", DotNetRid = "win-x64", DotNetNativeName = "NodeJS" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.NodeJS.NativeAssets.Linux", DotNetRid = "linux-x64", DotNetNativeName = "NodeJS" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.NodeJS.NativeAssets.macOS", DotNetRid = "osx", DotNetNativeName = "NodeJS" },
+
+                new ProjectAndRidWithNativeName { Name = "Puerts.QuickJS.NativeAssets.Win32", DotNetRid = "win-x64", DotNetNativeName = "QuickJS" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.QuickJS.NativeAssets.Linux", DotNetRid = "linux-x64", DotNetNativeName = "QuickJS" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.QuickJS.NativeAssets.macOS", DotNetRid = "osx", DotNetNativeName = "QuickJS" },
+
+                new ProjectAndRidWithNativeName { Name = "Puerts.V8.NativeAssets.Win32", DotNetRid = "win-x64", DotNetNativeName = "V8" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.V8.NativeAssets.Linux", DotNetRid = "linux-x64", DotNetNativeName = "V8" },
+                new ProjectAndRidWithNativeName { Name = "Puerts.V8.NativeAssets.macOS", DotNetRid = "osx", DotNetNativeName = "V8" },
+                ];
+        }
+
+        public class ProjectAndRidWithNativeName
+        {
+            /// <summary>
+            /// Gets the csproj name of the native project.
+            /// </summary>
+            public string Name { get; init; }
+            /// <summary>
+            /// Gets the RID (Runtime Identifier) of the related native project.
+            /// </summary>
+            public string DotNetRid { get; init; }
+            public string CmakeRid
+            {
+                get
+                {
+                    return DotNetRid switch
+                    {
+                        "win-x64" => "win-x64",
+                        "linux-x64" => "linux-x64",
+                        "osx" => "osx",
+                        _ => throw new NotSupportedException($"RID '{DotNetRid}' is not supported.")
+                    };
+                }
+            }
+            /// <summary>
+            /// Puerts.V8.NativeAssets.macOS => "V8"
+            /// </summary>
+
+            public string DotNetNativeName { get; init; }
+            public string CmakeNativeName
+            {
+                get
+                {
+                    return DotNetNativeName switch
+                    {
+                        "Core" => "puerts",
+                        "Lua" => "papi-lua",
+                        "NodeJS" => "papi-nodejs",
+                        "QuickJS" => "papi-quickjs",
+                        "V8" => "papi-v8",
+                        _ => throw new NotSupportedException($"NativeName '{DotNetNativeName}' is not supported.")
+                    };
+                }
+            }
+        }
+    }
+}
