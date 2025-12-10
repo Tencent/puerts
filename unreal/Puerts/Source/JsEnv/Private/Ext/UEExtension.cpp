5011c129baff900616fcdb6cd73a93ed62d138e9
赵烨 <2904276237@qq.com>
2025-12-10 10:58:49 +0800

    feat: 修改 tab 分隔符为空格

--- unreal/Puerts/Source/JsEnv/Private/Ext/UEExtension.cpp
+++ unreal/Puerts/Source/JsEnv/Private/Ext/UEExtension.cpp
@@ -82,8 +82,8 @@ static void FText_Format(const v8::FunctionCallbackInfo<v8::Value>& Info)
 
 static FText FromStringTable(const FName InTableId, const FString& InKey, const EStringTableLoadingPolicy InLoadingPolicy = EStringTableLoadingPolicy::FindOrLoad)
 {
-	// 这块代码主要是为了兼容 UE5.5 版本之后，FText::FromStringTable 函数的参数从 FString 改为 FTextKey 类型
-	return FText::FromStringTable(InTableId, InKey, InLoadingPolicy);
+    // 这块代码主要是为了兼容 UE5.5 版本之后，FText::FromStringTable 函数的参数从 FString 改为 FTextKey 类型
+    return FText::FromStringTable(InTableId, InKey, InLoadingPolicy);
 }
 
 #endif
