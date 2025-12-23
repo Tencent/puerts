655e2aae2348ae5a640a082729308a60f9a04ad2
realybin <realybin@outlook.com>
2025-12-23 00:49:48 +0000

    test

--- unity/nuget/build/Program.cs
+++ unity/nuget/build/Program.cs
@@ -251,6 +251,8 @@ public sealed class CollectNativeAssetsTask : FrostingTask<BuildContext>
             Directory.CreateDirectory(targetDirectory.FullPath);
 
             var files = context.GetFiles(new GlobPattern($"{nativeAssetsPath.FullPath}/**/*"));
+            Console.WriteLine($"Copying files from {nativeAssetsPath.FullPath} to {targetDirectory.FullPath}");
+            Console.WriteLine($"Files is {string.Join(", ", files.Select(f => f.FullPath))}");
             context.CopyFiles(files, targetDirectory.FullPath);
         }
     }
